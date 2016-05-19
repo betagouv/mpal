aa7bac3cfbdc720ecb88300d532c17428b1f9b7d
Christophe Robillard <geek@robiweb.net>
2016-05-19 07:36:23 +0200

    add create view for folder (not persistent for the moment)

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,22 @@
 class Folder
   include ActiveModel::Model
   attr_accessor :numero_fiscal, :reference_avis, :description
+  validates :numero_fiscal, :reference_avis, :revenu_reference, presence: true
+
+  def owner
+    @owner ||= api_particulier.owner
+  end
+
+  def revenu_reference
+    @revenu_reference ||= api_particulier.revenu_reference
+  end
+
+  def anah_eligible?
+    self.revenu_reference < 14308
+  end
+
+  private
+    def api_particulier
+      @api_particulier ||= ApiParticulier.new(reference_avis, numero_fiscal)
+    end
 end
---
+++ app/services/api_particulier.rb
@@ -0,0 +1,37 @@
+class ApiParticulier
+  def initialize(reference_avis, numero_fiscal)
+    @reference_avis = reference_avis
+    @numero_fiscal = numero_fiscal
+  end
+
+  def revenu_reference
+    get("revenuFiscalReference")
+  end
+
+  def owner
+    "#{first_name} #{last_name}"
+  end
+
+  private
+    def get(attribute)
+      @response ||= HTTParty.get("#{base_uri}/api/impots/svair?numeroFiscal=#{@numero_fiscal}&referenceAvis=#{@reference_avis}",
+        headers: headers)
+      JSON.parse(@response.body)[attribute]
+    end
+
+    def headers
+      { 'X-API-KEY' => 'test-token', 'accept' => "application/json" }
+    end
+
+    def base_uri
+      'https://apiparticulier-test.sgmap.fr'
+    end
+
+    def first_name
+      get("declarant1")['prenoms']
+    end
+
+    def last_name
+      get("declarant1")['nom']
+    end
+end
