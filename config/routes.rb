6ae6465526e6cd70667747d5916296f173a22817
Christophe Robillard <geek@robiweb.net>
2016-05-19 08:18:23 +0200

    wording: folder => project

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   scope(path_names: { new: 'nouveau' }) do
-    resources :folders, only: [:new, :create], path: 'dossiers'
+    resources :folders, only: [:new, :create], path: 'projets'
   end
 end 
