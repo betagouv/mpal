06fec2e665ebe76d892e0e25c962e1eeaafc0a27
Christophe Robillard <geek@robiweb.net>
2016-05-28 16:05:35 +0200

    add new contact modal form in projet view

--- spec/features/projet_spec.rb
+++ spec/features/projet_spec.rb
@@ -10,4 +10,16 @@ describe "Projet", type: :feature do
     click_button "Démarrez votre projet"
     expect(page).to have_content("Martin")
   end
+
+  scenario "ajout d'un acteur non référencé" do
+    visit projet_path(projet)
+    fill_in :nouveau_contact_name, with: 'Syndic de la Mare'
+    fill_in :nouveau_contact_email, with: 'syndic@lamare.com'
+    choose 'operator'
+    fill_in :nouveau_contact_message, with: "J'attends de vous ..."
+    click_button "J'invite ce nouveau contact"
+    expect(page).to have_content('Syndic de la Mare')
+    expect(page).to have_content('syndic')
+    expect(page).to have_content("J'attends une réponse")
+  end
 end
