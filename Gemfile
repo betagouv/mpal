a8d3da03eb01f1ba6e166bc106c989885c282fc9
Christophe Robillard <geek@robiweb.net>
2016-06-02 08:22:34 +0200

    add dotenv

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'dotenv-rails'
 end
 
 group :development do
--- app/services/api_particulier.rb
+++ app/services/api_particulier.rb
@@ -1,10 +1,14 @@
 class ApiParticulier
-  HEADERS = { 'X-API-KEY' => 'test-token', 'accept' => "application/json" }
-  BASE_URI = 'https://apiparticulier-test.sgmap.fr'
+  HEADERS = { 'X-API-KEY' => ENV['API_PARTICULIER_KEY'], 'accept' => "application/json" }
+  DOMAIN = ENV['API_PARTICULIER_DOMAIN']
 
   def retrouve_contribuable(numero_fiscal, reference_avis)
-    response = HTTParty.get("#{BASE_URI}/api/impots/svair?numeroFiscal=#{numero_fiscal}&referenceAvis=#{reference_avis}", headers: HEADERS)
+    response = HTTParty.get(uri(numero_fiscal, reference_avis), headers: HEADERS)
     response.code == 200 ? Contribuable.new(JSON.parse(response.body)) : nil
+  end
+
+  private def uri(numero_fiscal, reference_avis)
+    "https://#{DOMAIN}/api/impots/svair?numeroFiscal=#{numero_fiscal}&referenceAvis=#{reference_avis}"
   end
 end
 
