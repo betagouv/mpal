2ddbda1ef34413795ee0216cd8e4c55b5f795d01
Christophe Robillard <geek@robiweb.net>
2016-06-02 08:02:47 +0200

    add marker

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  var mymap = L.map('mapid').setView([45.778878,4.837361], 11);
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(mymap);
+  if ($("#mapid").length) {
+//    var mymap = L.map('mapid').setView([45.778878,4.837361], 11);
+    coord = { 'lat': 48.810357, lng: 2.392986 };      
+    var mymap = L.map('mapid').setView([coord.lat, coord.lng], 16);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(mymap);
+    L.marker([coord.lat, coord.lng]).addTo(mymap);
+  }
 
 });
