cc553dac7746bcea67339ec24c8cc1c082227b30
Christophe Robillard <geek@robiweb.net>
2016-06-04 23:13:21 +0200

    ajout du test de la description pour le démarrage d'un projet

--- spec/features/projet_spec.rb
+++ spec/features/projet_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 describe "Projet", type: :feature do
   let(:projet) { FactoryGirl.create(:projet) }
-  scenario "initialisation du projet" do
+  scenario "je démarre mon projet" do
     visit new_projet_path
     fill_in :projet_numero_fiscal, with: '12'
     fill_in :projet_reference_avis, with: '15'
     fill_in :projet_description, with: "Je veux changer ma chaudière"
     click_button "Démarrez votre projet"
     expect(page).to have_content("Martin")
+    expect(page).to have_content("Je veux changer ma chaudière")
   end
 
   scenario "ajout d'un acteur non référencé", js: true do
