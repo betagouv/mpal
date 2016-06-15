84c4a29297dc389587549c78643880172616dd31
Christophe Robillard <geek@robiweb.net>
2016-06-15 11:21:27 +0200

    ajout du lien sur le projet dans mail d'invitation operateur

--- spec/mailers/projet_mailer_spec.rb
+++ spec/mailers/projet_mailer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe ProjetMailer do
   describe "invitation operateur" do
-    let(:invitation) { FactoryGirl.build(:invitation) }
+    let(:invitation) { FactoryGirl.create(:invitation) }
     let(:email) { ProjetMailer.invitation_operateur(invitation) }
     it { expect(email.from).to eq(['no-reply@mpal.beta.gouv.fr']) }
     it { expect(email.to).to eq([invitation.operateur_email]) }
@@ -10,5 +10,6 @@ describe ProjetMailer do
     it { expect(email.body.encoded).to match(invitation.usager) }
     it { expect(email.body.encoded).to match(invitation.adresse) }
     it { expect(email.body.encoded).to match(invitation.description) }
+    it { expect(email.body.encoded).to match(invitation_url(jeton_id: invitation.token)) }
   end
 end
