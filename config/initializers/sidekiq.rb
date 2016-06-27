2e5e9408cff14d05c40e2051514b38dc63f0a8a5
Christophe Robillard <geek@robiweb.net>
2016-06-27 18:10:47 +0200

    Modif REDIS_URL en local

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,7 @@
-require 'dotenv'
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDIS_URL"], namespace: "sidekiq-#{Rails.env}" }
+  config.redis = { url: (ENV["REDIS_URL"] || 'redis://localhost:6379/0') , namespace: "sidekiq-#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDIS_URL"], namespace: "sidekiq-#{Rails.env}" }
+  config.redis = { url: (ENV["REDIS_URL"] || 'redis://localhost:6379/0') , namespace: "sidekiq-#{Rails.env}" }
 end
