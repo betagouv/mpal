b626e9cb17028f067a04c7af3306b8c828b73ff3
Christophe Robillard <geek@robiweb.net>
2016-06-29 08:55:30 +0200

    refactoring

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,25 +5,29 @@ class SessionsController < ApplicationController
   end
 
   def create
-    service = ApiParticulier.new
-    contribuable = service.retrouve_contribuable(params[:numero_fiscal], params[:reference_avis])
+    contribuable = ApiParticulier.new.retrouve_contribuable(params[:numero_fiscal], params[:reference_avis])
     if contribuable
       session[:numero_fiscal] = params[:numero_fiscal]
       projet = ProjetEntrepot.par_numero_fiscal(params[:numero_fiscal])
-      unless projet 
-        facade = ProjetFacade.new(service, ApiBan.new)
-        projet = facade.initialise_projet(params[:numero_fiscal], params[:reference_avis])
-        projet.save
-        notice = t('projets.messages.creation.corps')
-        flash[:notice_titre] = t('projets.messages.creation.titre', usager: projet.usager)
-      end
       if projet
-        redirect_to projet, notice: notice
-      else
-        redirect_to new_session_path, alert: t('sessions.erreur_generique')
+        redirect_to projet
+      else 
+        create_projet_and_redirect
       end
     else 
       redirect_to new_session_path, alert: t('sessions.invalid_credentials')
+    end
+  end
+  
+  def create_projet_and_redirect
+    facade = ProjetFacade.new(ApiParticulier.new, ApiBan.new)
+    projet = facade.initialise_projet(params[:numero_fiscal], params[:reference_avis])
+    if projet.save
+      notice = t('projets.messages.creation.corps')
+      flash[:notice_titre] = t('projets.messages.creation.titre', usager: projet.usager)
+      redirect_to projet_path(projet), notice: notice
+    else
+      redirect_to new_session_path, alert: t('sessions.erreurs.creation_projet')
     end
   end
 end
