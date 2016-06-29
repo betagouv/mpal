1aca99c16d2a7bdacc97111f73731b2a9b660c0f
Christophe Robillard <geek@robiweb.net>
2016-06-29 15:38:09 +0200

    commentaires pour un intervenant

--- app/controllers/commentaires_controller.rb
+++ app/controllers/commentaires_controller.rb
@@ -3,12 +3,8 @@ class CommentairesController < ApplicationController
   def create
     commentaire = @projet_courant.commentaires.build(corps_message: params[:commentaire][:corps_message])
     commentaire.auteur = @utilisateur_courant
-    if commentaire.save
-      redirect_to commentaire.projet
-    else
-      raise "ERROR: #{commentaire.errors.full_messages}"
-      
-    end 
+    commentaire.save
+    redirect_to projet_path(commentaire.projet, jeton: params[:jeton])
   end
 
 end
--- spec/features/commentaire_spec.rb
+++ spec/features/commentaire_spec.rb
@@ -7,14 +7,23 @@ feature 'commentaire' do
   let(:commentaire) { FactoryGirl.create(:commentaire) }
   let(:projet) { commentaire.projet }
   let(:message) { "Vous ne m'avez toujours pas répondu." }
+  let(:invitation) { FactoryGirl.create(:invitation) }
 
-  scenario "ajout d'un commentaire" do
+  scenario "ajout d'un commentaire par un demandeur" do
     signin(projet.numero_fiscal, projet.reference_avis)
     visit projet_path(projet)
 
     fill_in :commentaire_corps_message, with: message
     click_button I18n.t('projets.visualisation.lien_ajout_commentaire')
     expect(page).to have_content(message)
+  end
+
+  scenario "ajout d'un commentaire par un intervenant" do
+    visit projet_path(id: invitation.projet, jeton: invitation.token)
+    fill_in :commentaire_corps_message, with: message
+    click_button I18n.t('projets.visualisation.lien_ajout_commentaire')
+    expect(page).to have_content(message)
+
   end
 
 end
