9768d228ff37ea172f951f346fb4526d0d3fb521
Christophe Robillard <geek@robiweb.net>
2016-06-29 22:47:53 +0200

    ajout mailer mise_en_relation

--- app/mailers/projet_mailer.rb
+++ app/mailers/projet_mailer.rb
@@ -5,4 +5,9 @@ class ProjetMailer < ActionMailer::Base
     @invitation = invitation
     mail(to: invitation.intervenant_email, subject: t('mailers.projet_mailer.invitation_intervenant.sujet', usager: @invitation.usager))
   end
+
+  def mise_en_relation_intervenant(invitation)
+    @invitation = invitation
+    mail(to: invitation.intervenant_email, subject: t('mailers.projet_mailer.mise_en_relation_intervenant.sujet', usager: @invitation.usager))
+  end
 end
--- spec/mailers/projet_mailer_spec.rb
+++ spec/mailers/projet_mailer_spec.rb
@@ -12,4 +12,13 @@ describe ProjetMailer do
     it { expect(email.body.encoded).to match(invitation.description) }
     xit { expect(email.body.encoded).to match(projet_url(invitation.projet, jeton: invitation.token)) }
   end
+
+  describe "mise en relation intervenant" do
+    let(:mise_en_relation) { FactoryGirl.create(:mise_en_relation) }    
+    let(:email) { ProjetMailer.mise_en_relation_intervenant(mise_en_relation) }
+    it { expect(email.from).to eq(['no-reply@mpal.beta.gouv.fr']) }
+    it { expect(email.to).to eq([mise_en_relation.intervenant_email]) }
+    it { expect(email.subject).to eq(I18n.t('mailers.projet_mailer.mise_en_relation_intervenant.sujet', usager: mise_en_relation.usager)) }
+  end 
+
 end
