68c298426c971317431720f42f1f05cfe3317fe4
Christophe Robillard <geek@robiweb.net>
2016-06-30 10:46:03 +0200

    locales pour mise en relation

--- app/mailers/projet_mailer.rb
+++ app/mailers/projet_mailer.rb
@@ -8,6 +8,6 @@ class ProjetMailer < ActionMailer::Base
 
   def mise_en_relation_intervenant(invitation)
     @invitation = invitation
-    mail(to: invitation.intervenant_email, subject: t('mailers.projet_mailer.mise_en_relation_intervenant.sujet', usager: @invitation.usager))
+    mail(to: invitation.intervenant_email, subject: t('mailers.projet_mailer.mise_en_relation_intervenant.sujet', intermediaire: @invitation.intermediaire))
   end
 end
