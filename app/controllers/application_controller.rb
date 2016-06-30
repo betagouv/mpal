c6d3c7ec100bec79a8421f93c3098622de91926f
Laurent Bossavit <laurent.git@bossavit.com>
2016-06-30 12:46:42 +0200

    Fix Clavis authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+module CASClient
+  module XmlResponse
+    alias_method :check_and_parse_xml_normally, :check_and_parse_xml
+    def check_and_parse_xml(raw_xml)
+      cooked_xml = raw_xml.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+      check_and_parse_xml_normally(cooked_xml)
+    end
+  end
+end
