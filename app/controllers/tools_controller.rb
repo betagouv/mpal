3367a07f490c1d8a68a1615919cac6138a3b6059
JC <blondel.jc@gmail.com>
2016-07-06 10:57:20 +0200

    Copie du staging/production pour tester l'affichage du bouton reset

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -2,10 +2,12 @@ class ToolsController < ApplicationController
   skip_before_action :authentifie
 
   def reset_base
-    Evenement.delete_all
-    Occupant.delete_all
-    Invitation.delete_all
-    Projet.delete_all
-    redirect_to root_path, notice: t('reinitialisation.succes')
+    unless Rails.env.production?
+      Evenement.delete_all
+      Occupant.delete_all
+      Invitation.delete_all
+      Projet.delete_all
+      redirect_to root_path, notice: t('reinitialisation.succes')
+    end
   end
 end
