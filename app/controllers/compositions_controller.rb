fbb0ee7ebbd33c1d55dc3fe3f7127c57a4fcfe38
JC <blondel.jc@gmail.com>
2016-07-11 15:49:13 +0200

    Test pour le nb_occupants_a_charge du projet + mise en avance de données de composition logement directement dans le show du projet

--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -5,7 +5,10 @@ class CompositionsController < ApplicationController
 
   def update
     @projet_courant.nb_occupants_a_charge = params[:projet][:nb_occupants_a_charge]
-    @projet_courant.save
-    redirect_to edit_projet_composition_path(@projet_courant), notice: t('projets.composition_logement.messages.succes')
+    if @projet_courant.save
+      redirect_to edit_projet_composition_path(@projet_courant), notice: t('projets.composition_logement.messages.succes')
+    else
+      render :edit, notice: @projet_courant.errors
+    end
   end
 end
