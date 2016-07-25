56eade5313818424ab00a45d6f785cbc1708d825
Christophe Robillard <geek@robiweb.net>
2016-07-25 13:42:40 +0200

    remplace plan financement (subventions) si plan existe déjà

--- app/controllers/api/subventions_controller.rb
+++ app/controllers/api/subventions_controller.rb
@@ -2,12 +2,15 @@ module API
   class SubventionsController < ActionController::Base
     def create
       projet = Projet.find(params[:projet_id]) 
+      plan_financements_existe = projet.subventions.any?
+      projet.subventions.destroy_all
       subventions_json = JSON.parse(request.body.read)
       subventions_json.each do |subvention|
         projet.subventions.build(subvention)
       end
       projet.save
-      render nothing: true, status: :created
+      status = plan_financements_existe ? :ok : :created
+      render nothing: true, status: status
     end
   end
 end
--- spec/controllers/api/subventions_controller_spec.rb
+++ spec/controllers/api/subventions_controller_spec.rb
@@ -4,8 +4,8 @@ require 'support/api_particulier_helper'
 require 'support/api_ban_helper'
 
 describe API::SubventionsController do
+  let(:projet) { FactoryGirl.create(:projet) }
   describe "ajoute un plan de financmenet" do 
-    let(:projet) { FactoryGirl.create(:projet) }
     it "ajoute un plan de financement pour un projet donné" do
       libelle = 'CD92 - Aide amélioration habitat privé' 
       plan = [ { libelle: libelle, montant: 2400.50 } ]
@@ -13,5 +13,16 @@ describe API::SubventionsController do
       expect(response.status).to eq(201)
       expect(projet.subventions.first.libelle).to eq(libelle)
     end
+  end
+
+  it "remplace un plan de travaux pour un projet avec un plan existant" do
+      plan = [ { libelle: 'CD92 - Aide amélioration habitat privé', montant: 2400.50 } ]
+      plan2 = [ { libelle: 'Anah - Aide amélioration habitat privé', montant: 1400.10 } ]
+      post :create, plan.to_json, projet_id: projet.id, "CONTENT_TYPE": 'application/json'
+      post :create, plan2.to_json, projet_id: projet.id, "CONTENT_TYPE": 'application/json'
+      expect(response.status).to eq(200)
+      expect(projet.subventions.first.libelle).to eq("Anah - Aide amélioration habitat privé")
+      expect(projet.subventions.count).to eq(1)
+    
   end
 end
