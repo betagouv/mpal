3838b5e823e2dde44c5a71e5b7964b8d33f83c64
annemarie35 <annemariee@free.fr>
2016-08-02 15:09:14 +0200

    Stocke en local les fichiers uploadés, si on est en environnement de dev ou de test

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -1,3 +1,2 @@
 class DocumentUploader < CarrierWave::Uploader::Base
-  storage :fog
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,16 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'OpenStack',
-    openstack_username: ENV['OS_USERNAME'],
-    openstack_api_key: ENV['OS_API_KEY'],
-    openstack_auth_url: ENV['OS_AUTH_URL'],
-    openstack_region: ENV['OS_REGION']
-  }
-  config.fog_directory = ENV['OS_CONTAINER']
+  if Rails.env.production?
+    config.fog_credentials = {
+      provider: 'OpenStack',
+      openstack_username: ENV['OS_USERNAME'],
+      openstack_api_key: ENV['OS_API_KEY'],
+      openstack_auth_url: ENV['OS_AUTH_URL'],
+      openstack_region: ENV['OS_REGION']
+    }
+
+    config.fog_directory = ENV['OS_CONTAINER']
+    config.storage = :fog
+  else
+    config.storage = :file
+  end
 end
