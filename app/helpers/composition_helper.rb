0a2b192e577aeb4915a849e03bbf56a2d710313c
annemarie35 <annemariee@free.fr>
2016-08-25 10:09:08 +0200

    Réusine le bouton pour modifier un occupant en vue d'une refactorisation future

--- app/helpers/composition_helper.rb
+++ app/helpers/composition_helper.rb
@@ -1,15 +1,15 @@
 module CompositionHelper
 
-  def demandeur
+  def demandeur?
     @role_utilisateur == :demandeur
   end
 
   def bouton_ajout_occupant
-    if demandeur
+    if demandeur?
       html = <<-HTML
       <div class="ui small button">
         <i class="add icon"></i>
-        #{link_to t('projets.visualisation.lien_ajout_occupant'), new_projet_occupant_path(@projet_courant) if demandeur}
+        #{link_to t('projets.visualisation.lien_ajout_occupant'), new_projet_occupant_path(@projet_courant) if demandeur?}
       </div>
       HTML
       html.html_safe
@@ -17,15 +17,15 @@ module CompositionHelper
   end
 
   def bouton_suppression_occupant(occupant)
-    link_to 'Supprimer', projet_occupant_path(@projet_courant, occupant), method: :delete if demandeur
+    link_to 'Supprimer', projet_occupant_path(@projet_courant, occupant), method: :delete if demandeur?
   end
 
   def bouton_modification_occupant(occupant)
-    link_to 'Modifier', edit_projet_occupant_path(@projet_courant, occupant) if demandeur
+    link_to 'Modifier', edit_projet_occupant_path(@projet_courant, occupant) if demandeur?
   end
 
   def bouton_modifier_composition
-    if demandeur
+    if demandeur?
       html = <<-HTML
       <div class="ui right floated icon button">
         <i class="user icon"></i>
--- app/helpers/projet_helper.rb
+++ app/helpers/projet_helper.rb
@@ -1,16 +1,12 @@
 module ProjetHelper
 
-  def demandeur
+  def demandeur?
     @role_utilisateur == :demandeur
   end
 
   def bouton_modification_projet
-    if demandeur
+    if demandeur?
       link_to t('projets.visualisation.lien_edition'), edit_projet_path(@projet_courant)
     end
-  end
-
-  def bouton_modifier_demandeur
-    link_to 'Modifier', edit_projet_occupant_path(@projet_courant, @projet_courant.demandeur_principal) if demandeur
   end
 end
