b5231eb5368e18a86dd0cd55660e90d751f5acb7
annemarie35 <annemariee@free.fr>
2016-09-06 12:26:14 +0200

    Redirige vers la page projet lorsqu'on modifie le nombre d'occupants à charge close #85

--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -7,7 +7,7 @@ class CompositionsController < ApplicationController
   def update
     @projet_courant.nb_occupants_a_charge = params[:projet][:nb_occupants_a_charge]
     if @projet_courant.save
-      redirect_to edit_projet_composition_path(@projet_courant), notice: t('projets.composition_logement.messages.succes')
+      redirect_to projet_path(@projet_courant), notice: t('projets.composition_logement.messages.succes')
     else
       render :edit, notice: @projet_courant.errors
     end
