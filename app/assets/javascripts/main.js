162aeaf87a7266f97f00f9e16bb7db56546c2149
Yannick Francois <yaf@elsif.fr>
2016-09-21 22:51:42 +0200

    Change la façon d'attacher les événements dans le plan travaux

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,5 @@
-$(document).ready(function() {
+$(document).on('ready', 'page:load', function() {
   if ($("#mapid").length) {
-//    var mymap = L.map('mapid').setView([45.778878,4.837361], 11);
     coord = { 'lat': gon.latitude, lng: gon.longitude };
     var mymap = L.map('mapid').setView([coord.lat, coord.lng], 16);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -8,20 +7,19 @@ $(document).ready(function() {
         }).addTo(mymap);
     L.marker([coord.lat, coord.lng]).addTo(mymap);
   }
+});
 
+$(document).on('click', ".plan_travaux", function () {
+  var prestationId = $(this).data('prestation-id');
+  var url = $(this).parent().data('url');
+  var checkbox = $(this).children().first("checkbox");
+  var attributeName = checkbox.val();
+  var value = checkbox.prop("checked");
 
-  $(".plan_travaux").click(function () {
-    var prestationId = $(this).data('prestation-id');
-    var url = $(this).parent().data('url');
-    var checkbox = $(this).children().first("checkbox");
-    var attributeName = checkbox.val();
-
-    var value = checkbox.prop("checked");
-
-    $.post(url, {prestation_id: prestationId, attributeName: attributeName, value: value}, function (data) {
-        console.log("ok");
-    }, function (data) {
-      console.log("ko ?");
-    });
+  $.post(url, {prestation_id: prestationId, attributeName: attributeName, value: value}, function (data) {
+    console.log("ok");
+  }, function (data) {
+    console.log("ko ?");
   });
 });
+
