1816b387ccea1cc5c09ecd39fca44ec7ac8c663e
Christophe Robillard <geek@robiweb.net>
2016-09-22 16:29:17 +0200

    Reusine la vue intervenants

--- app/controllers/intervenants_controller.rb
+++ app/controllers/intervenants_controller.rb
@@ -2,5 +2,6 @@ class IntervenantsController < ApplicationController
   def index
     @pris_disponibles = @projet_courant.intervenants_disponibles(role: :pris)
     @operateurs_disponibles = @projet_courant.intervenants_disponibles(role: :operateur).shuffle
+    render "index_#{@role_utilisateur}"
   end
 end
