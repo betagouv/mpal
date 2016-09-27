35ebf3fea7a606d7e0eef44bebf776459c1aa6d1
Yannick Francois <yaf@elsif.fr>
2016-09-27 22:10:46 +0200

    Précise un test sur la recherche d'intervenants

--- spec/models/intervenant_spec.rb
+++ spec/models/intervenant_spec.rb
@@ -4,7 +4,7 @@ describe Intervenant do
   it { expect(FactoryGirl.build(:intervenant)).to be_valid }
   it { expect(FactoryGirl.build(:intervenant, raison_sociale: '  ')).not_to be_valid }
   it { is_expected.to have_db_column(:informations) }
-  
+
   let!(:urbanos) { FactoryGirl.create(:intervenant, raison_sociale: 'Urbanos', departements: ['93', '75', '91'], roles:[:operateur]) }
   let!(:soliho) { FactoryGirl.create(:intervenant, raison_sociale: 'Soliho', departements: ['91', '77'], roles: [:operateur]) }
 
