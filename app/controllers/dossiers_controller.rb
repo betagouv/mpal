449edd624264bceec87563d799e37ba844317701
Christophe Robillard <geek@robiweb.net>
2016-10-07 07:07:32 +0200

    affiche le numero du dossier opal dans la page projet

--- app/controllers/dossiers_controller.rb
+++ app/controllers/dossiers_controller.rb
@@ -2,7 +2,7 @@ class DossiersController < ApplicationController
   def create
     if (@role_utilisateur == :intervenant) && (@utilisateur_courant.instructeur?)
       if Opal.new(OpalClient).creer_dossier(@projet_courant)
-        redirect_to(projet_path(@projet_courant), notice: t('projets.creation_opal.messages.succes', id_opal: @projet_courant.opal_id))
+        redirect_to(projet_path(@projet_courant), notice: t('projets.creation_opal.messages.succes', id_opal: @projet_courant.opal_numero))
       else
         redirect_to(projet_path(@projet_courant), alert: t('projets.creation_opal.messages.erreur'))
       end
