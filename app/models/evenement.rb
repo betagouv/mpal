867a86a9a60e0bd63fccb9e76b3c46f3dcbafc38
Amesteves <amesteves@octo.com>
2016-10-10 16:06:11 +0200

    affiche un message annonçant que le choix de l'opérateur vient d'être effectué dans la chronologie du projet

--- app/models/evenement.rb
+++ app/models/evenement.rb
@@ -15,6 +15,8 @@ class Evenement < ActiveRecord::Base
       I18n.t('mise_en_relation_intervenant', scope: 'evenements', intermediaire: self.producteur.intermediaire, intervenant: self.producteur.intervenant)
     when 'ajout_avis_imposition'
       I18n.t('ajout_avis_imposition', scope: 'evenements', avis_imposition: self.producteur.label)
+    when 'choix_intervenant'
+      I18n.t('choix_intervenant', scope: 'evenements', operateur: self.producteur.raison_sociale)
     end
   end
 
