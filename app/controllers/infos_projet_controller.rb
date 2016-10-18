ee7e53ea320fc5a4b0b6eeafa31af1368344c2f4
Amesteves <amesteves@octo.com>
2016-10-18 12:08:18 +0200

    Corrige bug d'affichage des pages statiques d'information de l'utilisateur

--- app/controllers/infos_projet_controller.rb
+++ app/controllers/infos_projet_controller.rb
@@ -1,10 +1,14 @@
-class InfosProjetController < ApplicationController
+class InfosProjetController < ActionController::Base
+
   def faq
+    render :layout => 'application'
   end
 
   def cgu
+    render :layout => 'application'
   end
 
   def mentions_legales
+    render :layout => 'application'
   end
 end
