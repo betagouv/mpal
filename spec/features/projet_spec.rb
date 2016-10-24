bb3e9767ea0f780bd189a263820470aa31270fb3
Amesteves <amesteves@octo.com>
2016-10-24 17:41:18 +0200

    Envoie un mail à l'opérateur lorsqu'il a été choisi par un demandeur. Test incomplet

--- spec/features/projet_spec.rb
+++ spec/features/projet_spec.rb
@@ -43,9 +43,10 @@ feature "Projet" do
   end
 
   scenario "choisit un opérateur qui a été invité" do
+    # cette spec est plus précise, je suggère de la déplacer dans spec/features/choisir_un_operateur_spec.rb
+    # il y a également un spc pour les invitations / même genre de tets spec/features/invitation_spec.rb
     operateur = FactoryGirl.create(:intervenant, :operateur)
-    invitation = FactoryGirl.create(:invitation, intervenant: operateur)
-    projet = invitation.projet
+    invitation = FactoryGirl.create(:invitation, projet: projet, intervenant: operateur)
     signin(projet.numero_fiscal, projet.reference_avis)
     visit projet_intervenants_path(projet)
     expect(page).to have_content(invitation.intervenant.raison_sociale)
