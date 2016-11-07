8b9715ab53b1097cb674bfde513eb004a0430c50
Christophe Robillard <geek@robiweb.net>
2016-11-07 11:48:02 +0100

    nettoie les tests inutilisés

--- spec/features/commentaire_spec.rb
+++ spec/features/commentaire_spec.rb
@@ -9,14 +9,6 @@ feature 'commentaire' do
   let(:message) { "Vous ne m'avez toujours pas répondu." }
   let(:invitation) { FactoryGirl.create(:invitation) }
 
-  scenario "ajout d'un commentaire par un demandeur", pending: true do
-    signin(projet.numero_fiscal, projet.reference_avis)
-    visit projet_suivi_path(projet)
-    fill_in :commentaire_corps_message, with: message
-    click_button I18n.t('projets.visualisation.lien_ajout_commentaire')
-    expect(page).to have_content(message)
-  end
-
   scenario "ajout d'un commentaire par un intervenant" do
     visit projet_path(id: invitation.projet, jeton: invitation.token)
     fill_in :commentaire_corps_message, with: message
--- spec/features/invitation_spec.rb
+++ spec/features/invitation_spec.rb
@@ -9,39 +9,6 @@ feature "Invitation" do
   let!(:operateur) { FactoryGirl.create(:intervenant, departements: [projet.departement], roles: [:operateur]) }
   let(:invitation) { FactoryGirl.create(:invitation) }
 
-  scenario "prise de contact avec un pris", pending: true do
-    signin(projet.numero_fiscal, projet.reference_avis)
-    click_link I18n.t('projets.visualisation.invitation_intervenant')
-    fill_in :projet_description, with: 'Je veux changer ma chaudière'
-    fill_in :projet_email, with: 'martin@gmel.com'
-    fill_in :projet_tel, with: '01 30 20 40 10'
-    click_button I18n.t('invitations.nouvelle.action', intervenant: pris.raison_sociale)
-    expect(page).to have_content(I18n.t('invitations.messages.succes', intervenant: pris.raison_sociale))
-    expect(page).to have_content('martin@gmel.com')
-    expect(page).to have_css '.invites'
-  end
-
-  scenario "prise de contact avec un pris avec un e-mail non valide", pending: true do
-    signin(projet.numero_fiscal, projet.reference_avis)
-    click_link I18n.t('projets.visualisation.invitation_intervenant')
-    fill_in :projet_description, with: 'Je veux changer ma chaudière'
-    fill_in :projet_tel, with: '01 30 20 40 10'
-        fill_in :projet_email, with: "lolo"
-    click_button I18n.t('invitations.nouvelle.action', intervenant: pris.raison_sociale)
-    expect(page).to have_content(I18n.t('invitations.messages.erreur'))
-    expect(page).to have_content(I18n.t('invitations.messages.email.obligatoire'))
-  end
-
-  scenario "prise de contact avec un pris sans laisser d'email", pending: true do
-    signin(projet.numero_fiscal, projet.reference_avis)
-    click_link I18n.t('projets.visualisation.invitation_intervenant')
-    fill_in :projet_description, with: 'Je veux changer ma chaudière'
-    fill_in :projet_tel, with: '01 30 20 40 10'
-    click_button I18n.t('invitations.nouvelle.action', intervenant: pris.raison_sociale)
-    expect(page).to have_content(I18n.t('invitations.messages.erreur'))
-    expect(page).to have_content(I18n.t('invitations.messages.email.obligatoire'))
-  end
-
   scenario "mise en relation par un pris entre un operateur et un demandeur" do
     invitation = FactoryGirl.create(:invitation, projet: projet, intervenant: pris)
     visit projet_path(id: invitation.projet, jeton: invitation.token)
