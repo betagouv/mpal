a8fa80e6d46cf402e4b773cb93288f498397ebd4
Anne-Marie Esteves <anne-marie.esteves@beta.gouv.fr>
2016-11-15 16:06:53 +0100

    Améliore la vue de l'étape 2, déscription du projet

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -4,6 +4,7 @@ class ToolsController < ApplicationController
   def reset_base
     if Tools.demo?
       Invitation.destroy_all
+      Demande.destroy_all
       Projet.destroy_all
       reset_session
       redirect_to root_path, notice: t('reinitialisation.succes')
