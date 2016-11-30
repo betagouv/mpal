71caeda25b5ce12a6c0e70d406c7fa752375a533
Anne-Marie Esteves <amesteves@OCTO-EST.paris.octo>
2016-11-30 15:48:34 +0100

    Corrige une coquille dans l'adresse du demandeur

--- spec/factories/projets.rb
+++ spec/factories/projets.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :projet do
     numero_fiscal 12
     reference_avis 15
-    adresse_ligne1 '12 rue de la mare'
+    adresse_ligne1 '12 rue de la Mare'
     departement '75'
     code_insee '75010'
     code_postal '75010'
