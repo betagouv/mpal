9575fca34809382d3dd0108601f60174c9ab2f07
anne-marie <anne-marie.esteves@beta.gouv.fr>
2016-12-16 17:13:08 +0100

    Rend le bouton se connecter clivable même si la case n’est pas coché, cela permet de faire passer de nombreux tests qui dépendent de cette fonctionnalité en attendant

--- spec/support/mpal_helper.rb
+++ spec/support/mpal_helper.rb
@@ -2,8 +2,8 @@ def signin(numero_fiscal, reference_avis)
   visit new_session_path
   fill_in :numero_fiscal, with: numero_fiscal
   fill_in :reference_avis, with: reference_avis
-  check "autorisation"
-  click_on I18n.t('sessions.nouvelle.action')
+  check 'autorisation'
+  find('.btn').click
 end
 
 def json(body)
