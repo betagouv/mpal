e9a182e359019237fa9eca65d3c1043c723e9696
anne-marie <anne-marie.esteves@beta.gouv.fr>
2016-12-16 17:18:53 +0100

    Supprime une ligne inutile

--- spec/support/mpal_helper.rb
+++ spec/support/mpal_helper.rb
@@ -2,7 +2,6 @@ def signin(numero_fiscal, reference_avis)
   visit new_session_path
   fill_in :numero_fiscal, with: numero_fiscal
   fill_in :reference_avis, with: reference_avis
-  check 'autorisation'
   find('.btn').click
 end
 
