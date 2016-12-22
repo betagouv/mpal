6832d3c562f234e575e99997a6e104ce6241b14d
anne-marie <anne-marie.esteves@beta.gouv.fr>
2016-12-22 15:08:51 +0100

    Ajoute un nouveau fichier de test pour le volet latéral de la vue projet

---
+++ spec/features/visualiser_volet_lateral_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+require 'support/mpal_helper'
+require 'support/api_particulier_helper'
+require 'support/api_ban_helper'
+
+feature "L'opérateur visualise les informations syntéthiques concernant le projet dans le volet gauche de la vue projet" do
+end
