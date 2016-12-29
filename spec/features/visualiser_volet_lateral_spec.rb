e4254d1c5b85676b4e5389664445c627dbd34854
anne-marie <anne-marie.esteves@beta.gouv.fr>
2016-12-29 12:53:49 +0100

    Ajoute des tests pour le volet gauche de visualisation des infos de contact du demandeur

--- spec/features/visualiser_volet_lateral_spec.rb
+++ spec/features/visualiser_volet_lateral_spec.rb
@@ -4,4 +4,19 @@ require 'support/api_particulier_helper'
 require 'support/api_ban_helper'
 
 feature "L'opérateur visualise les informations syntéthiques concernant le projet dans le volet gauche de la vue projet" do
+
+  scenario "Les informations personnelles sont visibles " do
+    signin(12,15)
+    projet = Projet.last
+    annee = 2015
+    plafond = projet.preeligibilite(annee)
+    visit projet_path(projet)
+      within '.personal-information' do
+        expect(page).to have_content(projet.demandeur_principal)
+        expect(page).to have_content(projet.tel)
+        expect(page).to have_content(projet.email)
+        expect(page).to have_content("Très Modeste")
+        expect(page).to have_content(projet.nb_total_occupants)
+      end
+  end
 end
