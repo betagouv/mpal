6f6a8e4a45690124f5bee39661e49386c18a1feb
Julien Vignolles <jvignolles@octo.com>
2017-01-13 16:38:49 +0100

    Fix boucle infinie sur login via Clavis

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,35 +1,48 @@
 class ApplicationController < ActionController::Base
+  layout 'logged_in'
+
   protect_from_forgery with: :exception
   before_action :authentifie
 
-  def authentifie
+  def authentifie_sans_redirection
     jeton = params[:jeton] || session[:jeton]
     if jeton
       session[:jeton] = jeton
       @role_utilisateur = :intervenant
       invitation = Invitation.find_by_token(jeton)
-      intervenant = invitation.intervenant
-      projet = invitation.projet
+      if invitation
+        intervenant = invitation.intervenant
+        projet = invitation.projet
+      end
       if invitation && (projet.prospect? || intervenant == projet.operateur || intervenant.instructeur?)
         @projet_courant = projet
         @utilisateur_courant = intervenant
       else
-        utilisateur_invalide = true
+        @utilisateur_invalide = true
       end
     elsif agent_signed_in?
-      @role_utilisateur = :intervenant
+      @role_utilisateur = :agent
       projet_id = params[:projet_id] || params[:id]
-      @projet_courant = Projet.find(projet_id)
+      @projet_courant = Projet.find_by_id(projet_id)
       @utilisateur_courant = current_agent
     else
       @role_utilisateur = :demandeur
       projet_id = params[:projet_id] || params[:id]
-      @projet_courant = Projet.find(projet_id)
-      @utilisateur_courant = @projet_courant.demandeur_principal
-      utilisateur_invalide = true if session[:numero_fiscal] != @projet_courant.numero_fiscal
+      @projet_courant = Projet.find_by_id(projet_id)
+      if @projet_courant
+        @utilisateur_courant = @projet_courant.demandeur_principal
+        @utilisateur_invalide = true if session[:numero_fiscal] != @projet_courant.numero_fiscal
+      end
     end
+    true
+  end
 
-    redirect_to new_session_path, alert: t('sessions.access_forbidden') if utilisateur_invalide
+  def authentifie
+    authentifie_sans_redirection
+    if @utilisateur_invalide
+      return redirect_to new_session_path, alert: t('sessions.access_forbidden')
+    end
+    true
   end
 
   def after_sign_in_path_for(resource)
@@ -37,16 +50,6 @@ class ApplicationController < ActionController::Base
       projet_path(Projet.find(projet_id))
     else
       stored_location_for(resource) || root_path
-    end
-  end
-end
-
-module CASClient
-  module XmlResponse
-    alias :check_and_parse_xml_normally :check_and_parse_xml
-    def check_and_parse_xml(raw_xml)
-      cooked_xml = raw_xml.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
-      check_and_parse_xml_normally(cooked_xml)
     end
   end
 end
---
+++ config/initializers/casclient_override.rb
@@ -0,0 +1,9 @@
+module CASClient
+  module XmlResponse
+    alias_method :check_and_parse_xml_normally, :check_and_parse_xml
+    def check_and_parse_xml(raw_xml)
+      cooked_xml = raw_xml.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+      check_and_parse_xml_normally(cooked_xml)
+    end
+  end
+end
