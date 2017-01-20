2b5c57d1bd21b173c4b424212ecd55ea280045ba
Julien Vignolles <jvignolles@octo.com>
2017-01-20 15:18:10 +0100

    Fixes TU sur emails

--- spec/mailers/projet_mailer_spec.rb
+++ spec/mailers/projet_mailer_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe ProjetMailer, type: :mailer do
   describe "invitation intervenant" do
-    let(:invitation) { FactoryGirl.create(:invitation) }
+    let(:invitation) { create :invitation }
     let(:email) { ProjetMailer.invitation_intervenant(invitation) }
-    it { expect(email.from).to eq(['no-reply@mpal.beta.gouv.fr']) }
+    it { expect(email.from).to eq([ENV['NO_REPLY_FROM']]) }
     it { expect(email.to).to eq([invitation.intervenant_email]) }
     it { expect(email.subject).to eq(I18n.t('mailers.projet_mailer.invitation_intervenant.sujet', demandeur_principal: invitation.demandeur_principal)) }
     it { expect(email.body.encoded).to match(invitation.demandeur_principal.to_s) }
@@ -14,28 +14,22 @@ describe ProjetMailer, type: :mailer do
   end
 
   describe "mise en relation intervenant" do
-    let(:mise_en_relation) { FactoryGirl.create(:mise_en_relation) }
+    let(:mise_en_relation) { create :mise_en_relation }
     let(:email) { ProjetMailer.mise_en_relation_intervenant(mise_en_relation) }
-    it { expect(email.from).to eq(['no-reply@mpal.beta.gouv.fr']) }
+    it { expect(email.from).to eq([ENV['NO_REPLY_FROM']]) }
     it { expect(email.to).to eq([mise_en_relation.intervenant_email]) }
     it { expect(email.subject).to eq(I18n.t('mailers.projet_mailer.mise_en_relation_intervenant.sujet', intermediaire: mise_en_relation.intermediaire)) }
   end
 
   describe "l'intervenant reçoit un e-mail lorsqu'il a été choisi par le demandeur" do
-   it "le mail contient un sujet" do
-     @projet = FactoryGirl.create(:projet)
-     operateur = FactoryGirl.create(:intervenant, :operateur)
-     invitation = FactoryGirl.create(:invitation, projet: @projet, intervenant: operateur)
-     @projet.operateur = operateur
-     email = ProjetMailer.notification_choix_intervenant(@projet)
-
-     expect(email.from).to eq(['no-reply@mpal.beta.gouv.fr'])
-     expect(email.to).to eq([@projet.operateur.email])
-     expect(email.subject).to eq(I18n.t('mailers.projet_mailer.notification_choix_intervenant.sujet', intervenant: operateur, demandeur_principal: @projet.demandeur_principal))
-     expect(email.body.encoded).to match(invitation.demandeur_principal.to_s)
-     expect(email.body.encoded).to include(projet_url(@projet, jeton: invitation.token))
+    let(:operateur) { create :intervenant, :operateur }
+    let(:projet) { create :projet, operateur: operateur }
+    let!(:invitation) { create :invitation, projet: projet, intervenant: operateur }
+    let(:email) { ProjetMailer.notification_choix_intervenant(projet) }
+    it { expect(email.from).to eq([ENV['NO_REPLY_FROM']]) }
+    it { expect(email.to).to eq([projet.operateur.email]) }
+    it { expect(email.subject).to eq(I18n.t('mailers.projet_mailer.notification_choix_intervenant.sujet', intervenant: operateur, demandeur_principal: projet.demandeur_principal)) }
+    it { expect(email.body.encoded).to match(invitation.demandeur_principal.to_s) }
+    it { expect(email.body.encoded).to include(projet_url(projet, jeton: invitation.token)) }
    end
-
-  end
-
 end
