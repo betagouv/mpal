ac9a3070fc43a33f5dce3ad8023c9679d4195898
Pierre de La Morinerie <kemenaran@gmail.com>
2017-01-30 16:21:21 +0100

    Vérifie que les variables d’environnement sont présentes au démarrage de l’app

---
+++ config/initializers/environment.rb
@@ -0,0 +1,14 @@
+# Ensure the environment variables defined in .env.sample
+# are present in the execution environment.
+#
+# This protects against out-to-date environment leading to
+# runtime errors.
+
+REFERENCE_ENV_FILE = File.join(Rails.root, '.env.sample')
+
+File.foreach(REFERENCE_ENV_FILE).with_index { |line, line_num|
+  env_var = line[/([A-Z_]*)/, 1]
+  if ENV[env_var].blank?
+    raise "Configuration error: `#{env_var}` is not present in the process environment variables (declared in `.env.sample##{line_num}`)"
+  end
+}
