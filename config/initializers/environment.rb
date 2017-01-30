36a2b1c69556046942b4ae032c256a17eecf1fe7
Pierre de La Morinerie <kemenaran@gmail.com>
2017-01-30 16:36:35 +0100

    Ignore la validation des variables d’environnement pendant les tests
    Ça évite d’avoir à déclarer toutes les variables d’env dans la CI.

--- config/initializers/environment.rb
+++ config/initializers/environment.rb
@@ -1,14 +1,15 @@
-# Ensure the environment variables defined in .env.sample
+# Ensure that the environment variables defined in .env.sample
 # are present in the execution environment.
 #
-# This protects against out-to-date environment leading to
-# runtime errors.
+# This protects against out-to-date environment leading to runtime errors.
 
-REFERENCE_ENV_FILE = File.join(Rails.root, '.env.sample')
+if ENV['RAILS_ENV'] != 'test'
+  REFERENCE_ENV_FILE = File.join(Rails.root, '.env.sample')
 
-File.foreach(REFERENCE_ENV_FILE).with_index { |line, line_num|
-  env_var = line[/([A-Z_]*)/, 1]
-  if ENV[env_var].blank?
-    raise "Configuration error: `#{env_var}` is not present in the process environment variables (declared in `.env.sample##{line_num}`)"
-  end
-}
+  File.foreach(REFERENCE_ENV_FILE).with_index { |line, line_num|
+    env_var = line[/([A-Z_]*)/, 1]
+    if ENV[env_var].blank?
+      raise "Configuration error: `#{env_var}` is not present in the process environment variables (declared in `.env.sample##{line_num}`)"
+    end
+  }
+end
