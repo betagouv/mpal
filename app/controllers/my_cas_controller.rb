17e3318a5951eb1d3d895452d7c9feff5c792224
Julien Vignolles <jvignolles@octo.com>
2017-01-31 14:35:43 +0100

    Ajout de la deconnexion de Clavis

--- app/controllers/my_cas_controller.rb
+++ app/controllers/my_cas_controller.rb
@@ -3,6 +3,10 @@ class MyCasController < Devise::CasSessionsController
   skip_before_action :authentifie
   before_action :delete_flash_error, only: :new
 
+  def cas_logout_url
+    "#{ENV['CLAVIS_URL']}logout?service=#{root_url}"
+  end
+
 private
   # Le login SSO redirige sur Clavis, le message d'erreur "You need to sign in
   # or sign up before continuing." n'est donc pas affiché et apparaissait après
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -236,7 +236,7 @@ Devise.setup do |config|
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  config.sign_out_via = :get
 
   config.cas_base_url = ENV['CLAVIS_URL']
 end
