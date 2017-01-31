89bff61ca411da1f022d7f0016214dd1cb9998b6
Julien Vignolles <jvignolles@octo.com>
2017-01-31 12:21:30 +0100

    Fix message d'erreur après authentification SSO (Clavis) réussie

--- app/controllers/my_cas_controller.rb
+++ app/controllers/my_cas_controller.rb
@@ -1,4 +1,13 @@
 class MyCasController < Devise::CasSessionsController
   skip_before_action :assert_projet_courant
   skip_before_action :authentifie
+  before_action :delete_flash_error, only: :new
+
+private
+  # Le login SSO redirige sur Clavis, le message d'erreur "You need to sign in
+  # or sign up before continuing." n'est donc pas affiché et apparaissait après
+  # une authentification réussie.
+  def delete_flash_error
+    flash.delete(:error)
+  end
 end
