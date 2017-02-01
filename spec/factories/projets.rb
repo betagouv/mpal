79b716737451b29ac7042b77f8070595234bca86
Pierre de La Morinerie <kemenaran@gmail.com>
2017-02-01 16:24:14 +0100

    Factories: créer les relations dans :after_build plutôt qu’:after_create
    - :after_build est appelé au build *et* au create (vu que le create
    commence par un build)
    - ça permet que les relations établies à ce moment soient correctement
    save au moment d’enregistrer l’objet.

--- spec/factories/projets.rb
+++ spec/factories/projets.rb
@@ -16,7 +16,7 @@ FactoryGirl.define do
     end
 
     trait :with_intervenants do
-      after(:create) do |projet, evaluator|
+      after(:build) do |projet, evaluator|
         create(:intervenant, :operateur,   departements: [projet.departement])
         create(:intervenant, :pris,        departements: [projet.departement])
         create(:intervenant, :instructeur, departements: [projet.departement])
@@ -24,7 +24,7 @@ FactoryGirl.define do
     end
 
     trait :with_invited_operateur do
-      after(:create) do |projet, evaluator|
+      after(:build) do |projet, evaluator|
         operateur = create(:intervenant, :operateur, departements: [projet.departement])
         create(:invitation, projet: projet, intervenant: operateur)
       end
