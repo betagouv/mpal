7e6d82006215353f0aea14fc4d84a0857b6b79b5
Pierre de La Morinerie <kemenaran@gmail.com>
2017-02-01 17:48:09 +0100

    Specs: extraction du test « Engagement avec un opérateur »

---
+++ spec/features/engagement_avec_operateur_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+require 'support/mpal_helper'
+
+feature "En tant que demandeur, je peux m'engager avec un opérateur" do
+  let(:projet)    { create(:projet, :with_invited_operateur) }
+  let(:operateur) { projet.invited_operateur }
+
+  scenario "je m'engage auprès d'un opérateur qui a été consulté", pending: true do
+    signin(projet.numero_fiscal, projet.reference_avis)
+    visit projet_path(projet)
+    click_link I18n.t('projets.visualisation.s_engager_avec_operateur')
+    click_button I18n.t('choix_intervenants.nouveau.action')
+
+    expect(page.current_path).to eq(projet_path(projet))
+    expect(page).to have_content(I18n.t('projets.intervenants.messages.succes_choix_intervenant'))
+    expect(page).not_to have_content(I18n.t('projets.visualisation.s_engager_avec_operateur'))
+    within '.projet-ope' do
+      expect(page).to have_content(operateur.raison_sociale)
+      expect(page).to have_content(I18n.t('projets.operateur_construit_proposition', operateur: operateur.raison_sociale))
+    end
+  end
+end
--- spec/features/projet_spec.rb
+++ spec/features/projet_spec.rb
@@ -54,24 +54,4 @@ feature "En tant que demandeur, j'ai accès aux données concernant mon projet" 
     expect(page).to have_content(1950)
     # TODO: tester la modification des travaux demandés
   end
-
-  scenario "s'engage auprès d'un opérateur qui a été consulté" do
-    skip
-    # cette spec est plus précise, je suggère de la déplacer dans spec/features/choisir_un_operateur_spec.rb
-    # il y a également un spc pour les invitations / même genre de tets spec/features/invitation_spec.rb
-    operateur = FactoryGirl.create(:operateur)
-    invitation = FactoryGirl.create(:invitation, projet: projet, intervenant: operateur)
-    signin(projet.numero_fiscal, projet.reference_avis)
-    visit projet_path(projet)
-    expect(page).to have_content(invitation.intervenant.raison_sociale)
-    click_link I18n.t('projets.visualisation.s_engager_avec_operateur')
-    click_button I18n.t('choix_intervenants.nouveau.action')
-    expect(page.current_path).to eq(projet_path(projet))
-    expect(page).to have_content(I18n.t('projets.intervenants.messages.succes_choix_intervenant'))
-    expect(page).not_to have_content(I18n.t('projets.visualisation.s_engager_avec_operateur'))
-    within '.projet-ope' do
-      expect(page).to have_content(operateur.raison_sociale)
-      expect(page).to have_content(I18n.t('projets.operateur_construit_proposition', operateur: operateur.raison_sociale))
-    end
-  end
 end
