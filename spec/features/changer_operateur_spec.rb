3d70d4b335994c7add35d53865e19d23c694edc9
Pierre de La Morinerie <kemenaran@gmail.com>
2017-02-01 16:54:53 +0000

    Specs: teste le changement d’un opérateur vers un PRIS

--- spec/features/changer_operateur_spec.rb
+++ spec/features/changer_operateur_spec.rb
@@ -25,6 +25,26 @@ feature "en tant que demandeur, je peux changer d'opérateur" do
       expect(page).to have_content(new_operateur.raison_sociale)
       expect(page).not_to have_content(previous_operateur.raison_sociale)
     end
+
+    scenario "je peux passer d'un opérateur à un PRIS" do
+      signin(projet.numero_fiscal, projet.reference_avis)
+      click_link I18n.t('projets.visualisation.changer_intervenant')
+
+      expect(page).not_to have_content(I18n.t('demarrage_projet.etape3_choix_intervenant.section_eligibilite'))
+      expect(page).to have_selector('.choose-operator.pris')
+      expect(page).to have_selector('.choose-operator.intervenant')
+      expect(page).to have_selector("#intervenant_#{projet.invited_operateur.id}[checked]")
+
+      previous_operateur = projet.invited_operateur
+      pris = projet.intervenants_disponibles(role: :pris).first
+
+      choose pris.raison_sociale
+      check I18n.t('agrements.autorisation_acces_donnees_intervenants')
+      click_button I18n.t('projets.edition.action')
+
+      expect(page).to have_content(pris.raison_sociale)
+      expect(page).not_to have_content(previous_operateur.raison_sociale)
+    end
   end
 
   context "après m'être engagé avec un opérateur" do
