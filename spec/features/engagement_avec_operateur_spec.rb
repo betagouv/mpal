58690b4e1079c1e2893c8b816ae16130f3e215e0
Pierre de La Morinerie <kemenaran@gmail.com>
2017-02-01 16:53:55 +0000

    Specs: extraction et correction du test « Engagement avec un opérateur »

--- spec/features/engagement_avec_operateur_spec.rb
+++ spec/features/engagement_avec_operateur_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 require 'support/mpal_helper'
+require 'support/api_particulier_helper'
 
 feature "En tant que demandeur, je peux m'engager avec un opérateur" do
   let(:projet)    { create(:projet, :with_invited_operateur) }
   let(:operateur) { projet.invited_operateur }
 
-  scenario "je m'engage auprès d'un opérateur qui a été consulté", pending: true do
+  scenario "je m'engage auprès d'un opérateur qui a été consulté" do
     signin(projet.numero_fiscal, projet.reference_avis)
     visit projet_path(projet)
     click_link I18n.t('projets.visualisation.s_engager_avec_operateur')
-    click_button I18n.t('choix_intervenants.nouveau.action')
+    click_button I18n.t('projets.visualisation.engagement_action')
 
     expect(page.current_path).to eq(projet_path(projet))
     expect(page).to have_content(I18n.t('projets.intervenants.messages.succes_choix_intervenant'))
