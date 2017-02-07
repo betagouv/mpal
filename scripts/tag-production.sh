ca9c2e2cfd23ca2aa6243b0f8b9d5484fd9b3dfc
Pierre de La Morinerie <kemenaran@gmail.com>
2017-02-07 16:56:54 +0100

    Scripts: désactivation du pager pour les commits à déployer

--- scripts/tag-production.sh
+++ scripts/tag-production.sh
@@ -3,7 +3,7 @@
 # CircleCI déploie automatiquement en production les tags de la forme `production-*`.
 
 echo "Changements depuis le dernier tag:"
-git log `git describe --tags --abbrev=0 HEAD^`..HEAD --oneline 
+GIT_PAGER=cat git log `git describe --tags --abbrev=0 HEAD^`..HEAD --oneline 
 
 read -p "Êtes-vous sûr de vouloir tagger ces commits pour la production ? [o/N]" -n 1 -r
 echo
