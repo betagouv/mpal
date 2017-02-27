5f1f18a67a19aa672f93eb5910955b3ac559cf55
Pierre de La Morinerie <kemenaran@gmail.com>
2017-02-27 14:16:10 +0000

    Concerns: ne raise pas si l’attribut est inexistant
    Si on raise, l’appli Rails ne peut pas booter sans base de donnée.

--- app/controllers/concerns/localized_model_concern.rb
+++ app/controllers/concerns/localized_model_concern.rb
@@ -5,7 +5,6 @@ module LocalizedModelConcern
     # Définit un setter qui convertit des nombres localisés en nombres US (parsables par Ruby).
     # Exemple : "1 200,25" -> "1200.25"
     def localized_numeric_setter(attribute)
-      raise "Attribute '#{attribute}' is not among the attributes of the model" unless attribute_names.include?(attribute.to_s)
       define_method :"#{attribute}=" do |value|
         if value.kind_of? String
           value = value
