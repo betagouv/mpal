f0dcf6d009dde0d721ac8adb5108854fb1090ef1
Pierre de La Morinerie <kemenaran@gmail.com>
2017-03-01 11:22:45 +0100

    Scripts: vérifie que `master` est à jour avant de tagger la prod

--- scripts/production-deploy.sh
+++ scripts/production-deploy.sh
@@ -5,23 +5,68 @@
 # Termine le script à la première commande en erreur
 set -e
 
-branch=$(git rev-parse --abbrev-ref HEAD)
-if [ "$branch" != "master" ]; then
-  echo "Seuls les commits de la branche 'master' devraient être déployés en production – mais la branche actuelle est '$branch'."
-  echo "Placez-vous sur la branche 'master' pour continuer."
-  exit 1
-fi
+# Récupère le nom de la remote principale du projet (généralement 'origin' ou 'upstream')
+function remote_name {
+  git remote -v | grep "github.com.sgmap" | head -n 1 | cut -f 1
+}
 
-echo "Changements depuis le dernier tag:"
-GIT_PAGER=cat git log `git describe --tags --abbrev=0 HEAD^`..HEAD --oneline
+# Vérifie que le repo se trouve bien sur la branche à tagger en production.
+function assert_source_branch {
+  branch=$(git rev-parse --abbrev-ref HEAD)
+  if [ "$branch" != "$source_branch" ]; then
+    echo "La branche actuelle est '$branch' – mais seuls des commits de la branche '$source_branch' devraient être déployés en production."
+    echo "Placez-vous sur la branche '$source_branch' pour continuer."
+    exit 1
+  fi
+}
 
-read -p "Êtes-vous sûr de vouloir tagger ces commits pour la production ? [o/N]" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Oo]$ ]]
-then
-  REVISION=`git rev-parse --short HEAD`
-  git tag "production-${REVISION}"
-  echo "Le tag 'production-${REVISION}' a été créé."
+# Vérifie que la branche source est bien à jour avec le dépôt distant.
+function assert_branch_up_to_date {
+  git fetch "$remote_name"
+  local local_head=`git rev-parse ${source_branch}`
+  local remote_head=`git rev-parse ${remote_name}/${source_branch}`
+  if [[ "$local_head" != "$remote_head" ]]; then
+    echo "La branche locale '$source_branch' n’est pas à jour avec '${remote_name}/${source_branch}'."
+    echo "Récupérez les changements avec 'git pull' avant de continuer."
+    exit 1
+  fi
+}
+
+# Affiche les commits qui vont partir en production.
+function print_changelog {
+  echo "Changements depuis le dernier tag:"
+  GIT_PAGER=cat git log `git describe --tags --abbrev=0 HEAD^`..HEAD --oneline --no-merges
+}
+
+# Demande une confirmation avant de créer le tag.
+function confirm_tag {
+  read -p "Êtes-vous sûr de vouloir tagger ces commits pour la production ? [o/N]" -n 1 -r
+  echo
+  if ! [[ $REPLY =~ ^[Oo]$ ]]; then
+    exit 1
+  fi
+}
+
+# Crée le tag de production, et affiche un message avec les étapes suivantes.
+function create_production_tag {
+  local revision=`git rev-parse --short HEAD`
+  local date=`date +"%d/%m/%Y"`
+  local tag="production-${revision}-${date}"
+  git tag "$tag"
+
+  echo
+  echo "Le tag '${tag}' a été créé."
   echo "Maintenant utilisez 'git push --tags' pour publier le tag,"
   echo "et déclencher le déploiement en production."
-fi
+}
+
+# Configuration des branches
+source_branch="master"
+remote_name=$(remote_name)
+
+# Exécution du script
+assert_source_branch
+assert_branch_up_to_date
+print_changelog
+confirm_tag
+create_production_tag
