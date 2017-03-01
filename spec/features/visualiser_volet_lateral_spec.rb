a04c683f47e57017622cf20880ab959547aa4858
Julien Vignolles <jvignolles@octo.com>
2017-03-01 18:59:17 +0100

    ETQ demandeur, je ne dois pas voir mon type de revenus dans la colonne de gauche

--- spec/features/visualiser_volet_lateral_spec.rb
+++ spec/features/visualiser_volet_lateral_spec.rb
@@ -3,12 +3,27 @@ require 'support/mpal_features_helper'
 require 'support/api_particulier_helper'
 require 'support/api_ban_helper'
 
-feature "L'opérateur visualise les informations syntéthiques concernant le projet dans le volet gauche de la vue projet" do
-  let(:projet) { create :projet }
+feature "Les information personnelles syntéthiques sont visibles" do
+  let(:projet)          { create :projet }
+  let(:operateur)       { create :operateur, departements: [projet.departement] }
+  let(:agent_operateur) { create :agent, intervenant: operateur }
+  let!(:invitation)     { create :invitation, intervenant: operateur, projet: projet }
 
-  scenario "Les informations personnelles sont visibles" do
+  scenario "en tant que demandeur" do
     signin(projet.numero_fiscal, projet.reference_avis)
     visit projet_path(projet)
+    within '.personal-information' do
+      expect(page).to have_content(projet.demandeur_principal.fullname)
+      expect(page).to have_content(projet.tel)
+      expect(page).to have_content(projet.email)
+      expect(page).to have_no_content("très modeste")
+      expect(page).to have_content(I18n.t('projets.visualisation.occupants', count: projet.nb_total_occupants))
+    end
+  end
+
+  scenario "en tant qu'intervenant" do
+    login_as agent_operateur, scope: :agent
+    visit dossier_path(projet)
     within '.personal-information' do
       expect(page).to have_content(projet.demandeur_principal.fullname)
       expect(page).to have_content(projet.tel)
