ebed9f8363a5c6b77afd6915ae6ecfcc04d8a431
Pierre de La Morinerie <kemenaran@gmail.com>
2017-03-07 09:30:14 +0100

    Specs: refactore ApiParticulierHelper

--- spec/support/api_particulier_helper.rb
+++ spec/support/api_particulier_helper.rb
@@ -1,30 +1,45 @@
 require 'dotenv'
 
+module Fakeweb
+  class ApiParticulier
+    NUMERO_FISCAL  = 12
+    REFERENCE_AVIS = 15
+    INVALID = 'INVALID'
+
+    def self.register_success
+      FakeWeb.register_uri(
+        :get, "https://#{ENV['API_PARTICULIER_DOMAIN']}/api/impots/svair?numeroFiscal=#{NUMERO_FISCAL}&referenceAvis=#{REFERENCE_AVIS}",
+        content_type: 'application/json',
+        body: JSON.generate({
+          "declarant1": {
+            "nom": "Martin",
+            "prenoms": "Pierre",
+            "dateNaissance": "19/03/1980"
+          },
+          "anneeImpots": "2015",
+          "nombrePersonnesCharge": 2,
+          "foyerFiscal": {
+            "adresse": "12 rue de la Mare, 75010 Paris"
+          },
+          "revenuFiscalReference": 29880
+        }),
+      )
+    end
+
+    def self.register_invalid
+      FakeWeb.register_uri(
+        :get, "https://#{ENV['API_PARTICULIER_DOMAIN']}/api/impots/svair?numeroFiscal=#{INVALID}&referenceAvis=#{INVALID}",
+        content_type: 'application/json',
+        status: ["404", "Not Found"],
+        body: JSON.generate({ error: 'An error occured.'}),
+      )
+    end
+  end
+end
+
 RSpec.configure do |config|
   config.before(:each) do
-    FakeWeb.register_uri(
-      :get, "https://#{ENV['API_PARTICULIER_DOMAIN']}/api/impots/svair?numeroFiscal=12&referenceAvis=15",
-      content_type: 'application/json',
-      body: JSON.generate({
-        "declarant1": {
-          "nom": "Martin",
-          "prenoms": "Pierre",
-          "dateNaissance": "19/03/1980"
-        },
-        "anneeImpots": "2015",
-        "nombrePersonnesCharge": 2,
-        "foyerFiscal": {
-          "adresse": "12 rue de la Mare, 75010 Paris"
-        },
-        "revenuFiscalReference": 29880
-      }),
-    )
-
-    FakeWeb.register_uri(
-      :get, "https://#{ENV['API_PARTICULIER_DOMAIN']}/api/impots/svair?numeroFiscal=INVALID&referenceAvis=INVALID",
-      content_type: 'application/json',
-      status: ["404", "Not Found"],
-      body: JSON.generate({ error: 'An error occured.'}),
-    )
+    Fakeweb::ApiParticulier.register_success
+    Fakeweb::ApiParticulier.register_invalid
   end
 end
