8f306b3a9395ea488557d4fe8e189f23e58a2a03
Pierre de La Morinerie <kemenaran@gmail.com>
2017-03-08 14:35:27 +0100

    Specs: alignement

--- spec/support/mpal_features_helper.rb
+++ spec/support/mpal_features_helper.rb
@@ -2,7 +2,7 @@ require 'support/api_particulier_helper'
 
 def signin(numero_fiscal, reference_avis)
   visit new_session_path
-  fill_in :numero_fiscal, with: numero_fiscal
+  fill_in :numero_fiscal,  with: numero_fiscal
   fill_in :reference_avis, with: reference_avis
   find('.form-login .btn').click
 end
