6a3e99930a89d7f8cb44e66c668a43d0338b3b4e
Quentin Cattez <qcattez@octo.com>
2017-03-13 12:13:27 +0100

    modif contenu dashboard

--- spec/features/tableau_de_bord_spec.rb
+++ spec/features/tableau_de_bord_spec.rb
@@ -4,7 +4,7 @@ require 'support/api_particulier_helper'
 require 'support/api_ban_helper'
 
 feature "J'ai accès à mes dossiers depuis mon tableau de bord" do
-  let(:projet)      { create(:projet, :prospect, :with_intervenants_disponibles, :with_invited_operateur) }
+  let(:projet)      { create(:projet, :prospect, :with_invited_operateur) }
   let(:operateur)   { projet.invited_operateur }
   let(:instructeur) { create :instructeur, departements: [projet.departement] }
   let(:pris)        { create :pris,        departements: [projet.departement] }
@@ -28,7 +28,18 @@ feature "J'ai accès à mes dossiers depuis mon tableau de bord" do
 
   context "en tant qu'instructeur" do
     let(:agent) { create :agent, intervenant: instructeur }
-    # TODO
+
+    # Attention les instructeurs voient tous les dossiers ! En attente de la règle métier
+    scenario "je peux accéder à un dossier à partir du tableau de bord" do
+      visit dossiers_path
+      within "#projet_#{projet.id}" do
+        expect(page).to have_content(I18n.t("projets.statut.prospect"))
+      end
+
+      click_link projet.demandeur_principal.fullname
+      expect(page.current_path).to eq(dossier_path(projet))
+      expect(page).to have_content(projet.demandeur_principal.fullname)
+    end
   end
 
   context "en tant que PRIS" do
