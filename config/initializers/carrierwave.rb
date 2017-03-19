6eec69be1e635ac5ea50bf8572d0777ddc119a15
Julien Vignolles <jvignolles@octo.com>
2017-03-19 16:16:39 +0100

    FIX gemset env staging

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,11 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
       provider: 'OpenStack',
-      openstack_tenant: ENV['OS_TENANT_NAME'],
+      openstack_tenant:   ENV['OS_TENANT_NAME'],
       openstack_username: ENV['OS_USERNAME'],
-      openstack_api_key: ENV['OS_API_KEY'],
+      openstack_api_key:  ENV['OS_API_KEY'],
       openstack_auth_url: ENV['OS_AUTH_URL'],
-      openstack_region: ENV['OS_REGION']
+      openstack_region:   ENV['OS_REGION'],
     }
     config.fog_directory = ENV['OS_CONTAINER']
     config.storage = :fog
@@ -15,4 +15,4 @@ CarrierWave.configure do |config|
   end
 end
 
-CarrierWave::SanitizedFile.sanitize_regexp = /[^\w\.\-\+]/
+#CarrierWave::SanitizedFile.sanitize_regexp = /[^\w\.\-\+]/
