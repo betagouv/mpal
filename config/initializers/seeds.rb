06cede1eac3be3b5426281339794399d8d47fd54
Pierre de La Morinerie <kemenaran@gmail.com>
2017-05-10 18:22:06 +0200

    seeds: ne seede pas la base pendant les tâches rake [skip ci]

--- config/initializers/seeds.rb
+++ config/initializers/seeds.rb
@@ -5,7 +5,43 @@
 # But such a hook doesn't exist on Scalingo yet (although it does
 # on Heroku) ; hence this workaround.
 
-if ENV['IS_REVIEW_APP'] == "true" && !ActiveRecord::Migrator.needs_migration?
-  Rails.application.load_tasks
-  Rake::Task['db:seed'].invoke
+module ReviewApp
+  extend self
+
+  def seed_if_needed
+    # DEBUG
+    [:review_app?, :rake_task?, :db_ready?, :db_needs_seeds?].each do |selector|
+      begin
+        puts "#{selector.to_s} " + send(selector).to_s
+      rescue Exception => e
+        puts "#{selector.to_s} <Exception>"
+      end
+    end
+
+    if review_app? && !rake_task? && db_ready? && db_needs_seeds?
+      Rails.application.load_seed
+    end
+  end
+
+private
+
+  def review_app?
+    ENV['IS_REVIEW_APP'] == "true"
+  end
+
+  def rake_task?
+    File.split($0).last == 'rake'
+  end
+
+  def db_ready?
+    ActiveRecord::Migrator.needs_migration?
+  rescue ActiveRecord::NoDatabaseError
+    false
+  end
+
+  def db_needs_seeds?
+    Intervenant.count == 0
+  end
 end
+
+ReviewApp.seed_if_needed
