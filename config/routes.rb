0976bf0459d726515386ebe2f70baee0f071fb95
Julien Vignolles <jvignolles@octo.com>
2017-06-05 23:52:23 +0200

    PF514 ajoute la réinitialisation du mot de passe du demandeur

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, controllers: {
+    passwords:     "users/passwords",
     registrations: "users/registrations",
     sessions:      "users/sessions",
   }
