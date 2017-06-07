f24acf849d8f27c31150cf94cac5859861640fa0
Julien Vignolles <jvignolles@octo.com>
2017-06-07 16:22:13 +0200

    PF572 ajuste le bouton de contact et préremplit le formulaire

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,22 @@ class ContactsController < ApplicationController
 
   def new
     @contact = Contact.new
+    if current_user
+      @contact.email = current_user.email
+      project = current_user.projet
+      if project
+        @contact.name = project.demandeur.try(:fullname)
+        @contact.phone = project.tel
+      end
+    elsif current_agent
+      @contact.email = current_agent.username
+      @contact.name = current_agent.fullname
+    elsif session[:project_id]
+      project = Projet.find_by_id(session[:project_id])
+      @contact.name = project.demandeur.try(:fullname)
+      @contact.email = project.email
+      @contact.phone = project.tel
+    end
     @contact.subject = "J’ai une question…" if @contact.subject.blank?
     render_new
   end
