f35976a00f599aa88606a7f24fd4919d2ecfd8c8
Julien Vignolles <jvignolles@octo.com>
2017-06-08 17:16:03 +0200

    PF514 FIX saisie du nouveau mot de passe

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -10,25 +10,25 @@ class Users::PasswordsController < Devise::PasswordsController
   end
 
   # POST /resource/password
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
+  def edit
+    @page_heading = "Changement de mot de passe"
+    super
+  end
 
   # PUT /resource/password
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
-  # protected
-
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
+protected
+  def after_resetting_password_path_for(resource)
+    projet_path(resource.projet)
+  end
 
   # The path used after sending reset password instructions
   def after_sending_reset_password_instructions_path_for(resource_name)
