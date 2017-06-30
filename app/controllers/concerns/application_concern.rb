f86eaaa672054d3b1ccd168ef98521618c7fd63f
Quentin Cattez <qcattez@octo.com>
2017-06-30 17:25:57 +0200

    PF544 ajoute les actions du registre de paiements

--- app/controllers/concerns/application_concern.rb
+++ app/controllers/concerns/application_concern.rb
@@ -84,6 +84,7 @@ module ApplicationConcern
     expose_routing_helper :projet_or_dossier_occupants_path
     expose_routing_helper :projet_or_dossier_occupant_path
     expose_routing_helper :projet_or_dossier_demande_path
+    expose_routing_helper :projet_or_dossier_payment_registry_path
   end
 end
 
