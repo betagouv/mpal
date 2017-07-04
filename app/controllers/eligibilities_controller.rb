9891ee6a7305a0e431e2be9f9a4ff27b2a8e2116
Aryana Pezé <aryana.peze@octo.com>
2017-07-04 11:16:16 +0200

    PF620 met à jour locked_at lors de la decision de l'eligibilité

--- app/controllers/eligibilities_controller.rb
+++ app/controllers/eligibilities_controller.rb
@@ -5,5 +5,6 @@ class EligibilitiesController < ApplicationController
 
   def show
     @eligible = @projet_courant.preeligibilite(@projet_courant.annee_fiscale_reference) != :plafond_depasse
+    @projet_courant.update!(locked_at: Time.now)
   end
 end
