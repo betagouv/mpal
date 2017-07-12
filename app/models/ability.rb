b894f6ecf364168a4918769b0401509998d673bc
Aryana Pezé <aryana.peze@octo.com>
2017-07-12 16:50:40 +0200

    PF633 refacto de la page ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,41 +5,49 @@ class Ability
     agent_or_user ||= User.new
 
     define_payment_registry_abilities(agent_or_user, projet)
+
     if agent_or_user.is_a? Agent
       if agent_or_user.admin?
         can :manage, :all
-      elsif agent_or_user.pris? && projet.present?
-        can [:read, :recommander_operateurs], Projet if ( projet.statut.to_sym == :prospect && projet.invited_pris == agent_or_user.intervenant )
-      elsif agent_or_user.instructeur? && projet.present?
-        (can :read, Projet) if ( !projet.status_not_yet(:transmis_pour_instruction) &&  projet.invited_instructeur == agent_or_user.intervenant )
-      elsif agent_or_user.operateur? && (projet.try(:contacted_operateur) == agent_or_user.intervenant)
-        if (projet.statut.to_sym == :prospect)
+        return
+      end
+
+      if projet.blank?
+        can :index, Projet
+        return
+      end
+
+      if agent_or_user.pris? && projet.statut.to_sym == :prospect && projet.invited_pris == agent_or_user.intervenant
+        can [:read, :recommander_operateurs], Projet
+      elsif agent_or_user.instructeur? && !projet.status_not_yet(:transmis_pour_instruction) &&  projet.invited_instructeur == agent_or_user.intervenant
+        can :read, Projet
+      elsif agent_or_user.operateur? && projet.contacted_operateur == agent_or_user.intervenant
+        if projet.statut.to_sym == :prospect
           can :read, Projet
-        elsif (projet.statut.to_sym != :prospect) && projet.status_not_yet(:transmis_pour_instruction)
+        elsif projet.statut.to_sym != :prospect && projet.status_not_yet(:transmis_pour_instruction)
           can :manage, AvisImposition
           can :manage, Demande
           can :manage, :demandeur
           can :manage, Occupant
           can :manage, Projet
-        elsif (projet.statut.to_sym != :en_cours) && (projet.statut.to_sym != :prospect)
+        elsif projet.statut.to_sym != :en_cours && projet.statut.to_sym != :prospect
           can :read, Projet
         end
-      return
       end
     end
+
     if agent_or_user.is_a? User
-      if projet
-        if projet.locked_at.nil?
-          can :manage, AvisImposition
-          can :manage, Demande
-          can :manage, :eligibility
-          can :manage, :demandeur
-          can :manage, Occupant
-          can :manage, Projet
-        else
-          can :read, Projet
-          can :read, :eligibility
-        end
+      return if projet.blank?
+      if projet.locked_at.nil?
+        can :manage, AvisImposition
+        can :manage, Demande
+        can :manage, :eligibility
+        can :manage, :demandeur
+        can :manage, Occupant
+        can :manage, Projet
+      else
+        can :read, Projet
+        can :read, :eligibility
       end
     end
   end
