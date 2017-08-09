cdc57d33efc19124eb7ebd27408f02bef0c8c8e5
Aryana Pezé <aryana.peze@octo.com>
2017-08-09 15:50:25 +0200

    PF676 reformule certaines abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -24,17 +24,17 @@ class Ability
         can :read, Occupant
         can :read, Projet
       end
-      if agent_or_user.pris? && projet.statut.to_sym == :prospect && projet.invited_pris == agent_or_user.intervenant
+      if agent_or_user.pris? && projet.statut.to_sym == :prospect && project_belongs_to_pris(projet, agent_or_user)
         can [:read, :recommander_operateurs], Projet
       elsif agent_or_user.instructeur?
-        if !projet.status_not_yet(:transmis_pour_instruction) &&  projet.invited_instructeur == agent_or_user.intervenant
+        if !projet.status_not_yet(:transmis_pour_instruction) && project_belongs_to_instructeur(projet, agent_or_user)
           can :read, Projet
         end
-        if projet.statut.to_sym == :transmis_pour_instruction &&  projet.invited_instructeur == agent_or_user.intervenant
+        if projet.statut.to_sym == :transmis_pour_instruction && project_belongs_to_instructeur(projet, agent_or_user)
           can :manage, :dossiers_opal
         end
 
-      elsif agent_or_user.operateur? && projet.contacted_operateur == agent_or_user.intervenant
+      elsif agent_or_user.operateur? && project_belongs_to_operateur(projet, agent_or_user)
         if projet.statut.to_sym == :prospect
           can :read, Projet
         elsif projet.statut.to_sym != :prospect && projet.status_not_yet(:transmis_pour_instruction)
@@ -49,7 +49,7 @@ class Ability
       end
     end
 
-    if agent_or_user.is_a? User
+    if agent_or_user.is_a?(User)
       return if projet.blank?
       if projet.locked_at.nil?
         can :manage, AvisImposition
@@ -58,7 +58,7 @@ class Ability
         can :manage, :eligibility
         can :manage, Occupant
         can :manage, Projet
-      else
+      elsif project_belongs_to_demandeur(projet, agent_or_user)
         can :read, :eligibility
         can :read, Projet
       end
@@ -84,7 +84,7 @@ private
     alias_action :edit, :update, to: :modify
     alias_action :edit, :update, :destroy, to: :modify_destroy
     if projet.payment_registry.present?
-      if agent_or_user.try(:operateur?)
+      if agent_or_user.try(:operateur?) && project_belongs_to_operateur(projet, agent_or_user)
         can :add,                  Payment
         can :read,                 Payment, payment_registry_id: projet.payment_registry.id
         can :destroy,              Payment, payment_registry_id: projet.payment_registry.id, statut: ["en_cours_de_montage", "propose"], action: ["a_rediger", "a_modifier"]
@@ -92,13 +92,13 @@ private
         can :ask_for_validation,   Payment, payment_registry_id: projet.payment_registry.id, action: ["a_rediger", "a_modifier"] unless projet.status_not_yet(:en_cours_d_instruction)
       end
 
-      if agent_or_user.try(:instructeur?)
+      if agent_or_user.try(:instructeur?) && project_belongs_to_instructeur(projet, agent_or_user)
         can :read,                 Payment, payment_registry_id: projet.payment_registry.id, statut: ["demande", "en_cours_d_instruction", "paye"]
         can :ask_for_modification, Payment, payment_registry_id: projet.payment_registry.id, action: "a_instruire"
         can :send_in_opal,         Payment, payment_registry_id: projet.payment_registry.id, action: "a_instruire"
       end
 
-      if agent_or_user.is_a? User
+      if agent_or_user.is_a?(User) && project_belongs_to_demandeur(projet, agent_or_user)
         can :read,                 Payment, payment_registry_id: projet.payment_registry.id, statut: ["propose", "demande", "en_cours_d_instruction", "paye"]
         can :ask_for_modification, Payment, payment_registry_id: projet.payment_registry.id, action:  "a_valider"
         can :ask_for_instruction,  Payment, payment_registry_id: projet.payment_registry.id, action:  "a_valider"
@@ -109,19 +109,35 @@ private
   def define_document_abilities(agent_or_user, projet)
     alias_action :new,  :create, to: :add
     alias_action :edit, :update, to: :modify
-    
-    if agent_or_user.try(:operateur?) && projet.contacted_operateur == agent_or_user.intervenant && !projet.status_not_yet(:en_cours)
+
+    if agent_or_user.try(:operateur?) && project_belongs_to_operateur(projet, agent_or_user) && !projet.status_not_yet(:en_cours)
       can :add, Document
       can :modify, Document, projet_id: projet.id
       can :destroy, Document, id: projet.documents.map { |doc| doc.id }
     end
 
-    if agent_or_user.try(:instructeur?) && (!projet.status_not_yet(:transmis_pour_instruction) &&  projet.invited_instructeur == agent_or_user.intervenant )
+    if agent_or_user.try(:instructeur?) && project_belongs_to_instructeur(projet, agent_or_user) && !projet.status_not_yet(:transmis_pour_instruction)
       can :read, Document, id: projet.documents.map { |doc| doc.id }
     end
 
-    if agent_or_user.is_a?(User) && projet.locked_at.present?
+    if agent_or_user.is_a?(User) && project_belongs_to_demandeur(projet, agent_or_user)
       can :read, Document, id: projet.documents.map { |doc| doc.id }
     end
+  end
+
+  def project_belongs_to_operateur(projet, agent)
+    projet.contacted_operateur == agent.intervenant
+  end
+
+  def project_belongs_to_instructeur(projet, agent)
+    projet.invited_instructeur == agent.intervenant
+  end
+
+  def project_belongs_to_pris(projet, agent)
+    projet.invited_pris == agent.intervenant
+  end
+
+  def project_belongs_to_demandeur(projet, user)
+    projet.locked_at.present? && user == projet.user
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,7 @@ describe User do
     subject(:ability) { Ability.new(user, projet) }
 
     context "quand un projet est vérouillé" do
-      let(:projet) { create :projet, locked_at: Time.new(1789, 7, 14, 16, 0, 0) }
+      let(:projet) { create :projet, locked_at: Time.new(1789, 7, 14, 16, 0, 0), user: user }
 
       it { is_expected.not_to be_able_to(:manage, AvisImposition) }
       it { is_expected.not_to be_able_to(:manage, Demande) }
@@ -27,7 +27,7 @@ describe User do
     end
 
     context "quand un projet n'est pas encore vérouillé" do
-      let(:projet) { create :projet, :transmis_pour_instruction, :with_payment_registry }
+      let(:projet) { create :projet, :transmis_pour_instruction, user: user }
 
       it { is_expected.not_to be_able_to(:read, Document) }
 
@@ -40,7 +40,7 @@ describe User do
     end
 
     context "quand un registre de paiement existe" do
-      let(:projet) { create :projet, :transmis_pour_instruction, :with_payment_registry }
+      let(:projet) { create :projet, :transmis_pour_instruction, :with_payment_registry, locked_at: Time.new(1789, 7, 14, 16, 0, 0), user: user }
 
       let(:payment_en_cours_de_montage)    { create :payment, statut: :en_cours_de_montage }
       let(:payment_propose)                { create :payment, statut: :propose }
