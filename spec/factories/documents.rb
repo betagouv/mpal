87241809eb2f8173b6df9e45e47dc1b538fb5b6c
Quentin Cattez <qcattez@octo.com>
2017-08-09 15:51:55 +0200

    PF638 met à jour les tests pour les documents

--- spec/factories/documents.rb
+++ spec/factories/documents.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :document do
     projet
-    label "Titre de propriété"
     fichier { Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/fixtures/Ma pièce jointe.txt'))) }
   end
 end
