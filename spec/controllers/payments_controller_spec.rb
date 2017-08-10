38925216c41dbbcb772487ff537e3e4074f3539e
Aryana Pezé <aryana.peze@octo.com>
2017-08-10 16:55:44 +0200

    soucis orga des specs résolu

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -161,26 +161,25 @@ describe PaymentsController do
         expect(response).to redirect_to projet_payment_registry_path(projet)
       end
     end
+  end
+
+  describe "en tant que demandeur" do
+    let(:projet)            { create :projet, :en_cours_d_instruction, :with_payment_registry }
+    let(:agent_instructeur) { projet.agent_instructeur }
+    let(:submit_time)       { DateTime.new(1980, 01, 01) }
+    let(:payment)           { create :payment, :demande, beneficiaire: "Emile Lévesque", payment_registry: projet.payment_registry, submitted_at: submit_time }
+
+    before do
+      authenticate_as_agent agent_instructeur
+      put :send_in_opal, dossier_id: projet.id, payment_id: payment.id
+      payment.reload
+    end
 
     describe "#send_in_opal" do
-      let(:projet)            { create :projet, :en_cours_d_instruction, :with_payment_registry }
-      let(:agent_instructeur) { projet.agent_instructeur }
-      let(:submit_time)       { DateTime.new(1980, 01, 01) }
-      let(:payment)           { create :payment, :demande, beneficiaire: "Emile Lévesque", payment_registry: projet.payment_registry, submitted_at: submit_time  }
-
-      context "en tant qu'agent instructeur" do
-        before do
-          authenticate_as_agent agent_instructeur
-          put :send_in_opal, dossier_id: projet.id, payment_id: payment.id
-          projet.reload
-          payment.reload
-        end
-
-        it "transmet la demande de paiement dans Opal et met à jour la demande avec la r&ponse d'opal" do
-          expect(payment.montant).to eq 1000.12
-          expect(payment.statut).to eq "en_cours_d_instruction"
-          expect(response).to redirect_to dossier_payment_registry_path(projet)
-        end
+      it "transmet la demande de paiement dans Opal et met à jour la demande avec la r&ponse d'opal" do
+        expect(payment.montant).to eq 1000.12
+        expect(payment.statut).to eq "en_cours_d_instruction"
+        expect(response).to redirect_to dossier_payment_registry_path(projet)
       end
     end
   end
