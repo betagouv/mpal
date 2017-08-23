6ca6d3d0f337a262d26baa90b2ce5bc22122e875
Aryana Pezé <aryana.peze@octo.com>
2017-08-23 17:07:11 +0200

    quelques modifs en plus

--- app/controllers/intervenants_controller.rb
+++ app/controllers/intervenants_controller.rb
@@ -4,12 +4,14 @@ class IntervenantsController < ApplicationController
   authorize_resource :class => false
 
   def index
-    @page_heading = "Liste de contacts"
+    @page_heading = "Contacts"
+
     @pris = @projet_courant.invited_pris
     @operateur = @projet_courant.operateur
     @instructeur = @projet_courant.invited_instructeur
     @demandeur = @projet_courant.demandeur
     @personne = @projet_courant.personne
+
     if @personne && @personne.civilite == "mrs"
       @civilite = "Madame"
     else
