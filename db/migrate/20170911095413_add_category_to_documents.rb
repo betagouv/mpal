9c0d2cccbc84fd39f2d961708babe483079afae6
Quentin Cattez <qcattez@octo.com>
2017-09-13 16:20:43 +0200

    Fix migration polymorphisme sur document

--- db/migrate/20170911095413_add_category_to_documents.rb
+++ db/migrate/20170911095413_add_category_to_documents.rb
@@ -3,7 +3,7 @@ class AddCategoryToDocuments < ActiveRecord::Migration[5.1]
     add_reference :documents, :category, polymorphic: true, index: true
 
     Document.find_each do |document|
-      document.update!(category_id: document.projet.id, category_type: "Projet") if document.projet_id.present?
+      document.update!(category_id: document.projet_id, category_type: "Projet") if document.projet_id.present?
     end
 
     remove_reference :documents, :projet
@@ -14,10 +14,15 @@ class AddCategoryToDocuments < ActiveRecord::Migration[5.1]
 
     Document.find_each do |document|
       if document.category_type == "Projet"
-        document.update!(category_id: document.projet.id, category_type: "Projet")
+        document.update! projet_id: document.category_id
       elsif document.category_type == "Payment"
-        projet = document.category.try(:payment_registry).try(:projet)
-        document.update!(category_id: projet.id, category_type: "Projet") if projet.present?
+        payment = Payment.find_by_id document.category_id
+        if payment.present?
+          payment_registry = PaymentRegistry.find_by_id payment.payment_registry_id
+          if payment_registry.present? && payment_registry.projet_id.present?
+            document.update! projet_id: payment_registry.projet_id
+          end
+        end
       end
     end
 
