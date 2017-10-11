0d95e52e4536fc681a8a2c17a5d4b8b2fd62e068
Quentin Cattez <qcattez@octo.com>
2017-10-11 14:29:27 +0200

    Fix les coordonnées du PRIS dans les contacts

--- app/controllers/intervenants_controller.rb
+++ app/controllers/intervenants_controller.rb
@@ -5,27 +5,11 @@ class IntervenantsController < ApplicationController
 
   def index
     @page_heading = "Contacts"
-    eligible = @projet_courant.preeligibilite(@projet_courant.annee_fiscale_reference) != :plafond_depasse
-    fetch_pris
-    @operateur = @projet_courant.operateur
-    @instructeur = @projet_courant.invited_instructeur
-    @demandeur = @projet_courant.demandeur
-    @personne = @projet_courant.personne
-
-    if @personne && @personne.civilite == "mrs"
-      @civilite = "Madame"
-    else
-      @civilite = "Monsieur"
-    end
-  end
-
-private
-  def fetch_pris
-    if ENV['ROD_ENABLED'] == 'true'
-      rod_response = Rod.new(RodClient).query_for(@projet_courant)
-      @pris        = @eligible ? rod_response.pris : rod_response.pris_eie
-    else
-      @pris        = @projet_courant.intervenants_disponibles(role: :pris).first
-    end
+    @pris         = @projet_courant.invited_pris
+    @operateur    = @projet_courant.operateur
+    @instructeur  = @projet_courant.invited_instructeur
+    @demandeur    = @projet_courant.demandeur
+    @personne     = @projet_courant.personne
+    @civilite     = @projet_courant.personne&.civilite == "mrs" ? "Madame" : "Monsieur"
   end
 end
