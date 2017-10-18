233d4fb60f1cf3b94ebd310b9cdce70207f8a99b
Aryana Pezé <aryana.peze@octo.com>
2017-10-18 09:37:51 +0200

    PF814 permet de revenir au dossier

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,6 @@ class ContactsController < ApplicationController
   end
 
   def new
-
     @contact = Contact.new
     @subjects = Contact::SUBJECTS.map { |x| [I18n.t("contacts.subject_name.#{x}"), x] }
     if current_user
@@ -17,23 +16,23 @@ class ContactsController < ApplicationController
         #@contact.name  = current_user.infos.name
         #@contact.phone = current_user.infos.phone
       elsif current_user.demandeur?
-        projet                = current_user.projet_as_demandeur
-        @contact.name         = projet.demandeur.fullname
-        @contact.email        = current_user.email
-        @contact.phone        = projet.tel
-        @contact.department   = projet.adresse.departement
-        @contact.numero_plateforme = projet.numero_plateforme
+        @projet                    = current_user.projet_as_demandeur
+        @contact.name              = @projet.demandeur.fullname
+        @contact.email             = current_user.email
+        @contact.phone             = @projet.tel
+        @contact.department        = @projet.adresse.departement
+        @contact.numero_plateforme = @projet.numero_plateforme
       end
     elsif current_agent
       @contact.email = current_agent.username
       @contact.name  = current_agent.fullname
     elsif session[:project_id]
-      projet                = Projet.find_by_id(session[:project_id])
-      @contact.name         = projet.demandeur&.fullname
-      @contact.email        = projet.email
-      @contact.phone        = projet.tel
-      @contact.department   = projet.adresse&.departement
-      @contact.numero_plateforme = projet.numero_plateforme
+      @projet                    = Projet.find_by_id(session[:project_id])
+      @contact.name              = @projet.demandeur&.fullname
+      @contact.email             = @projet.email
+      @contact.phone             = @projet.tel
+      @contact.department        = @projet.adresse&.departement
+      @contact.numero_plateforme = @projet.numero_plateforme
     end
     render_new
   end
