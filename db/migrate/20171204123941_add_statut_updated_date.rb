78473f5186a6398513c3476056675bad27518679
glov <g.lovichi@groupeonepoint.com>
2017-12-04 14:15:49 +0100

    pf851 migration

--- db/migrate/20171204123941_add_statut_updated_date.rb
+++ db/migrate/20171204123941_add_statut_updated_date.rb
@@ -1,5 +1,5 @@
 class AddStatutUpdatedDate < ActiveRecord::Migration[5.1]
   def change
-  	add_column :projets, :statut_updated_date, :datetime
+    add_column :projets, :statut_updated_date, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026165651) do
+ActiveRecord::Schema.define(version: 20171204123941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -319,6 +319,7 @@ ActiveRecord::Schema.define(version: 20171026165651) do
     t.datetime "locked_at"
     t.datetime "date_depot"
     t.integer "max_registration_step", default: 1, null: false
+    t.datetime "statut_updated_date"
     t.index ["adresse_a_renover_id"], name: "index_projets_on_adresse_a_renover_id"
     t.index ["adresse_postale_id"], name: "index_projets_on_adresse_postale_id"
     t.index ["agent_instructeur_id"], name: "index_projets_on_agent_instructeur_id"
