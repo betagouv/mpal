3fec8469e03fd9f0a127bfe50c8abb56740ab3bd
glov <g.lovichi@groupeonepoint.com>
2017-12-13 15:38:17 +0100

    maj du message a l'operateur

--- app/controllers/dossiers_controller.rb
+++ app/controllers/dossiers_controller.rb
@@ -132,7 +132,7 @@ class DossiersController < ApplicationController
   def show
     list_department_intervenants
     if Time.now.strftime("%Y").to_i - @projet_courant.avis_impositions.first.annee.to_i >= 2 && current_agent.operateur? == true && @projet_courant.date_depot == nil
-      flash.now[:notice] = "Revenu Fiscal de Rerefence >= N-2"
+      flash.now[:notice] = "Veuillez modifier le RFR (cumulé) de ce dossier et indiquer la référence du(des) nouvel(eaux) avis dans les champs libres de la synthèse du dossier."
     end
     render_show
   end
