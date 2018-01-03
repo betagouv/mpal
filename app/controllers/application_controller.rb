44b2556868ce36b11b25b649c8003ea6f44ca854
Lucas SIRIGNANO <l.sirignano@groupeonepoint.com>
2018-01-03 16:53:19 +0100

    mail de confirmation design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,9 @@ class ApplicationController < ActionController::Base
     if projet_id = session[:projet_id_from_opal]
       projet_or_dossier_path(Projet.find_by_id(projet_id))
     else
-      resource.projets.first.update(:max_registration_step => Projet::STEP_MISE_EN_RELATION)
+      if resource.is_a?(User)
+        resource.projets.first.update(:max_registration_step => Projet::STEP_MISE_EN_RELATION)
+      end
       stored_location_for(resource) || root_path
     end
   end
