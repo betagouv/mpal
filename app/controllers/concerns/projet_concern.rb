69386ea0889b4aee911f72f4792511edded67ec5
m.meilland <m.meilland@groupeonepoint.com>
2018-01-25 10:43:07 +0100

    PF-763 Ergo recap projet it include PF-936, PF-662, PF-748, PF-437, PF-753, PF-749, PF-759

--- app/controllers/concerns/projet_concern.rb
+++ app/controllers/concerns/projet_concern.rb
@@ -17,6 +17,10 @@ module ProjetConcern
       @fundings_sum  = @public_aids_sum
       @fundings_sum += @projet_courant.personal_funding_amount || 0
       @fundings_sum += @projet_courant.loan_amount || 0
+      @fundings_sum += @private_aids_with_amounts.sum(:amount)
+      @remaining_sum = @projet_courant.global_ttc_sum
+      @remaining_sum -= @fundings_sum || 0
+      @global_ht_sum = @projet_courant.travaux_ht_amount || 0
       @intervenants_disponibles = @projet_courant.intervenants_disponibles(role: :operateur).shuffle
       @pris_departement = @projet_courant.intervenants_disponibles(role: :pris)
       @invitations_demandeur = Invitation.where(projet_id: @projet_courant.id)
