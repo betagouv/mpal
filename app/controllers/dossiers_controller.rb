9b927fb5f534dd34e52dfdf04ab9ba0cf10da020
Lucas SIRIGNANO <l.sirignano@groupeonepoint.com>
2018-02-16 17:08:39 +0100

    TRE

--- app/controllers/dossiers_controller.rb
+++ app/controllers/dossiers_controller.rb
@@ -378,7 +378,7 @@ class DossiersController < ApplicationController
       @rfrn2 = all.where("ift_avis_impositions2.annee is not NULL")
       if @rfrn2.limit(1).present?
         flash.now[:notice] = "Certains dossiers nécessitent de mettre à jour le ou les avis d'imposition (dernier avis d'imposition ou avis de situation déclarative disponible) (voir onglet RFR N-2)"
-        flash.now[:notice] = "</br>"
+        flash.now[:notice] += "\n"
       end
     elsif current_agent.instructeur?
       @traited = all.where("projets.actif = 1 and projets.statut >= 6")
