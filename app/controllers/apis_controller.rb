08ec8c4c47dca1a1c1773690274cb85244d8c98d
Gas Lovichi <g.lovichi@groupeonepoint.com>
2018-02-19 17:02:34 +0100

    PF822_OPAL-states

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -48,16 +48,22 @@ class ApisController < ApplicationController
 								opalNewStatut = "Reversement prononcé"
 							end
 
+							if dossier["position"] == "Lebowski"
+								opalNewStatut = "Lebowski"
+							end
+
+							
 							begin
 								opalDatePosition = DateTime.strptime(dossier["date"].to_s.slice(0..-4), '%s')
 							rescue
 								opalDatePosition = ""
 							end
 
+
 							if opalPositionLabel != "" && opalDatePosition != "" && opalDatePosition != nil && opalNewStatut != ""
 
-								if projet.opal_date_position != opalDatePosition || projet.opal_position_label != opalPositionLabel || projet.statut != opalNewStatut
-									projet.update(:opal_date_position => opalDatePosition, :opal_position_label => opalPositionLabel, :statut => opalNewStatut)
+								if projet.opal_date_position != opalDatePosition || projet.opal_position_label != opalPositionLabel || projet.opal_position != opalNewStatut
+									projet.update(:opal_date_position => opalDatePosition, :opal_position_label => opalNewStatut, :opal_position => opalPositionLabel)
 								end
 							end
 						end
