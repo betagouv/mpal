418fdbb57be8a465554bc282a7d353f85aa3cfa0
MCS <s.marie-celine@groupeonepoint.com>
2018-03-27 16:08:37 +0200

    fix bug demandeur seul

--- app/controllers/demandeur_seuls_controller.rb
+++ app/controllers/demandeur_seuls_controller.rb
@@ -9,24 +9,24 @@ class DemandeurSeulsController < ApplicationController
     gon.push({
         latitude:  @projet_courant.adresse.try(:latitude),
         longitude: @projet_courant.adresse.try(:longitude)
-      })
+    })
 
-      aids = @projet_courant.aids_with_amounts
-      @public_aids_with_amounts = aids.try(:public_assistance)
-      @private_aids_with_amounts = aids.try(:not_public_assistance)
+    aids = @projet_courant.aids_with_amounts
+    @public_aids_with_amounts = aids.try(:public_assistance)
+    @private_aids_with_amounts = aids.try(:not_public_assistance)
 
-      @public_aids_sum = @public_aids_with_amounts.sum(:amount)
-      @fundings_sum  = @public_aids_sum
-      @fundings_sum += @projet_courant.personal_funding_amount || 0
-      @fundings_sum += @projet_courant.loan_amount || 0
-      @fundings_sum += @private_aids_with_amounts.sum(:amount)
+    @public_aids_sum = @public_aids_with_amounts.sum(:amount)
+    @fundings_sum  = @public_aids_sum
+    @fundings_sum += @projet_courant.personal_funding_amount || 0
+    @fundings_sum += @projet_courant.loan_amount || 0
+    @fundings_sum += @private_aids_with_amounts.sum(:amount)
 
-      @remaining_sum = @projet_courant.global_ttc_sum
-      @remaining_sum -= @fundings_sum || 0
-      @global_ht_sum = @projet_courant.travaux_ht_amount || 0
-      @intervenants_disponibles = @projet_courant.intervenants_disponibles(role: :operateur).shuffle
-      @pris_departement = @projet_courant.intervenants_disponibles(role: :pris)
-      @invitations_demandeur = Invitation.where(projet_id: @projet_courant.id)
+    @remaining_sum = @projet_courant.global_ttc_sum
+    @remaining_sum -= @fundings_sum || 0
+    @global_ht_sum = @projet_courant.travaux_ht_amount || 0
+    @intervenants_disponibles = @projet_courant.intervenants_disponibles(role: :operateur).shuffle
+    @pris_departement = @projet_courant.intervenants_disponibles(role: :pris)
+    @invitations_demandeur = Invitation.where(projet_id: @projet_courant.id)
     render "projets/show_hma_ds" and return
   end
 
@@ -95,10 +95,8 @@ class DemandeurSeulsController < ApplicationController
     render "projets/proposition_hma_ds" and return
   end
 
-  private
-
   def assert_demande_seul
-    if ENV['ELIGIBLE_HMA'] != 'true' || !@projet_courant.demande.seul || !@projet.include?(current_user)
+    if !(ENV['ELIGIBLE_HMA'] == 'true' && @projet_courant.demande && @projet_courant.demande.seul && @projet_courant.users.include?(current_user))
       return redirect_to root_path, alert: t('sessions.access_forbidden')
     end
   end
