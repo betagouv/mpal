971f808c12711314fe1f117b9ac426445324abb7
Lucas SIRIGNANO <l.sirignano@groupeonepoint.com>
2018-04-04 17:58:18 +0200

    change system to be more organized

--- app/models/document.rb
+++ app/models/document.rb
@@ -26,37 +26,37 @@ class Document < ApplicationRecord
       hash[:none] << :autres_projet
     else
       hash = { required: [], one_of: [[:devis_projet, :estimation]], none: [:autres_projet] }
+      if projet_themes.include?("Autonomie")
+        hash[:required] << :justificatif_autonomie
+        hash[:required] << :diagnostic_autonomie
+      end
+
+      if projet_themes.include? ("Énergie")
+        hash[:required] << :evaluation_energetique
+        hash[:required] << :contrat_amo if projet.invited_pris.present? #projet.operation.present?
+      end
+
+      if projet_themes.include? ("SSH - petite LHI")
+        hash[:one_of] << [:arrete_insalubrite_peril, :rapport_grille_insalubrite, :arrete_securite, :justificatif_saturnisme]
+      end
+
+      if projet_themes.include? ("Travaux lourds")
+        hash[:required] << :evaluation_energetique
+        hash[:one_of]   << [:arrete_insalubrite_peril, :rapport_grille_insalubrite, :arrete_securite, :justificatif_saturnisme]
+
+        if projet.invited_pris.present? && !projet.hma.present? #projet.operation.present?
+          hash[:required] << :contrat_maitrise_oeuvre
+          hash[:required] << :contrat_amo
+        end
+      end
+
+      if projet_themes.include? ("Autres travaux")
+        hash[:one_of] << [:notification_agence_eau, :pv_copropriete]
+      end
     end
     #TODO Quand on aura intégré les mandats
     #hash[:required] << :mandat_projet if projet.mandat?
 
-    if projet_themes.include?("Autonomie") && projet.hma.nil?
-      hash[:required] << :justificatif_autonomie
-      hash[:required] << :diagnostic_autonomie
-    end
-
-    if projet_themes.include? ("Énergie") && projet.hma.nil?
-      hash[:required] << :evaluation_energetique
-      hash[:required] << :contrat_amo if projet.invited_pris.present? #projet.operation.present?
-    end
-
-    if projet_themes.include? ("SSH - petite LHI") && projet.hma.nil?
-      hash[:one_of] << [:arrete_insalubrite_peril, :rapport_grille_insalubrite, :arrete_securite, :justificatif_saturnisme]
-    end
-
-    if projet_themes.include? ("Travaux lourds") && projet.hma.nil?
-      hash[:required] << :evaluation_energetique
-      hash[:one_of]   << [:arrete_insalubrite_peril, :rapport_grille_insalubrite, :arrete_securite, :justificatif_saturnisme]
-
-      if projet.invited_pris.present? && projet.hma.nil? #projet.operation.present?
-        hash[:required] << :contrat_maitrise_oeuvre
-        hash[:required] << :contrat_amo
-      end
-    end
-
-    if projet_themes.include? ("Autres travaux") && projet.hma.nil?
-      hash[:one_of] << [:notification_agence_eau, :pv_copropriete]
-    end
 
     hash[:required] = hash[:required].uniq if hash[:required].present?
     hash[:one_of] = hash[:one_of].uniq if hash[:one_of].present?
