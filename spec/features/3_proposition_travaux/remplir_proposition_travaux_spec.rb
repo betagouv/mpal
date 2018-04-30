281bb70525a9a980780809dc83c27a41a1c9e8c7
MCS <s.marie-celine@groupeonepoint.com>
2018-04-30 14:01:58 +0200

    change spec

--- spec/features/3_proposition_travaux/remplir_proposition_travaux_spec.rb
+++ spec/features/3_proposition_travaux/remplir_proposition_travaux_spec.rb
@@ -174,6 +174,13 @@ feature "Remplir la proposition de travaux" do
       within ".test-document-panel-0-2-0" do
         attach_file :fichier, Rails.root + "spec/fixtures/Ma pièce jointe.txt"
         # click_button I18n.t("document.send")
+        form = find '#form-0-2-0'
+        class << form
+          def submit!
+            Capybara::RackTest::Form.new(driver, native).submit({})
+          end
+        end
+        form.submit!
       end
 
       document = projet.documents.first
