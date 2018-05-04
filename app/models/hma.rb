6b5aa24e5c107ca4b6b886b397b2ec7ccc5b6837
MCS <s.marie-celine@groupeonepoint.com>
2018-05-04 17:17:51 +0200

    hidden HMA DS

--- app/models/hma.rb
+++ app/models/hma.rb
@@ -1,7 +1,7 @@
 class Hma < ApplicationRecord
 	  belongs_to :projet
 
-	  validates :devis_ht, :devis_ttc, presence: true, on: :proposition_hma
+	  # validates :devis_ht, :devis_ttc, presence: true, on: :proposition_hma
 
 	  attr_accessor :localized_global_ttc_sum, :localized_global_ht_sum, :localized_ptz
 end
